5ede2037db300335ff0b86165bc9936454f7979d
Luca Fumis <lucaf@ebi.ac.uk>
2015-02-11 12:12:48 +0000

    Latest changes to gene-disease page to be merged with codebase

--- app/js/controllers.js
+++ app/js/controllers.js
@@ -17,6 +17,9 @@
     }]). 
 
 
+
+
+
     /**
        * GeneDiseaseCtrl
        * Controller for the Gene <-> Disease page
@@ -26,12 +29,19 @@
         $log.log('GeneDiseaseCtrl()');
 
         $scope.search = {
-    	   // target : $location.search().t,
-    	   // disease : $location.search().d,
-           info : {},
-           genetic_associations : {},
-           test : [2,4,4,3,4,7],
+            // target : $location.search().t,
+            // disease : $location.search().d,
+            info : {
+                data : {},
+                efo_path : []
+            },
+            genetic_associations : {},
+            rna_expression: [],
+            //test : [2,4,4,3,4,7],
+            test: [],
         };
+
+        //$scope.test = [];
 
         $scope.getInfo = function(){
             console.log("getInfo for "+$scope.search.target + " & " + $scope.search.disease);
@@ -42,8 +52,8 @@
                     size:1
                 } ).
                 success(function(data, status) {
-                    $scope.search.info = data.data[0];
-                    console.log("info:");
+                    $scope.search.info.data = data.data[0];
+                    console.log("info on association:");
                     console.log(data.data[0]);  
                 }).
                 error(function(data, status) {
@@ -53,7 +63,10 @@
 
 
 
+        // TODO:
+        // make the proper call to the API
         $scope.getEvidence = function(){
+            /*
             return cttvAPIservice.getAssociations( {
                     gene:$scope.search.target, 
                     efo:"http://identifiers.org/efo/"+$scope.search.disease.substring(4)
@@ -65,22 +78,139 @@
                 error(function(data, status) {
                     $log.error(status);
                 });
+            */
         }
 
 
+
+        // TODO:
+        // make the proper call, process the info and store it to the correct var (not 'test')
+        // which means this will return the promise object which wraps the setting of test...
+        $scope.getFlowerData = function(){
+            console.log("getFlowerData()");
+            $scope.search.test = [
+                {"value":6, "label":"Hello"},
+                {"value":3, "label":"There"},
+                {"value":5, "label":"World!"},
+                {"value":7, "label":"World!"},
+                {"value":4, "label":"World!"},
+                {"value":2, "label":"World!"}
+            ];
+        }
+
+
+
+        $scope.getRnaExpressionData = function(){
+            return cttvAPIservice.getAssociations( {
+                    gene:$scope.search.target, 
+                    efo:$scope.search.disease/*,
+                    size: 1000*/
+                } ).
+                success(function(data, status) {
+                    $scope.search.rna_expression = data.data;
+                    initTableRNA();
+                }).
+                error(function(data, status) {
+                    $log.error(status);
+                });
+        }
+
+
+
+        /*
+         * Takes the data object returned by the API and formats it to an array of arrays 
+         * to be displayed by the RNA-expression dataTable widget.
+         */
+        var formatRnaDataToArray = function(data){
+            var newdata = new Array(data.length);
+            console.log(data[0]);
+            for(var i=0; i<data.length; i++){
+                // create rows:
+                var row = [];
+                    // comparison
+                    row.push(data[i].evidence.experiment_specific.comparison_name);
+                    // activity
+                    row.push(data[i].biological_subject.properties.activity.split("/").pop().split("_").join(" "));
+                    // Tissue
+                    //row.push(data[i].biological_object.properties.biosamples.join(", ")); // is an array
+                    // fold change
+                    row.push(data[i].evidence.experiment_specific.log2_fold_change);
+                    // p-value
+                    row.push(data[i].evidence.association_score.pvalue.value);
+                    // provenance
+                    //row.push(data[i].evidence.urls.linkouts.reduce(function(p,c,a,i){return p.nice_name+", "+c.nice_name}));
+                    row.push("<a href='"+data[i].evidence.urls.linkouts[1].url+"' target='blank'>Gene expression details <i class='fa fa-external-link'></i></a>");
+                    // experiment overview
+                    row.push("<a href='"+data[i].evidence.urls.linkouts[0].url+"' target='blank'>Experiment overview and raw data <i class='fa fa-external-link'></i></a>");
+                    // publications
+                    // row.push(data[i].evidence.date_asserted);
+                    row.push("Estrogen receptor prevents p53-dependent apoptosis in breast cancer. Bailey ST, Shin H, Westerling T, Liu XS, Brown M. , Europe PMC 23077249"); // mock publications info
+
+                newdata[i] = row;
+            }
+
+            return newdata;
+        }
+
+
+
+        var initTableRNA = function(){
+            console.log("> dataTables:");
+            console.log($('#rna-expression-table'));
+
+            // make the call
+
+            $('#rna-expression-table').dataTable( {
+                "data": formatRnaDataToArray($scope.search.rna_expression), //[["non-small cell lung cancer", "decreased transcript level", "lung", "-1.07", "1.08e-17", "GPR65 expression details", "Transription profiling by array of human non-small cell lng cancer tissue", "bla bla bla"]],
+                "columns": [
+                    { "title": "Comparison" },
+                    { "title": "Activity" },
+                    //{ "title": "Tissue" },
+                    { "title": "log2 fold change" },
+                    { "title": "p-value" },
+                    { "title": "Provenance" },
+                    { "title": "Experiment overview" },
+                    { "title": "Publications" }
+                ],
+                "ordering" : true,
+                "autoWidth": false
+            } ); 
+        }
 
         if($location.search().t && $location.search().d){
             // parse parameters
             $scope.search.target = $location.search().t;
             $scope.search.disease = $location.search().d;
 
-            // need a way of parsing filters too...
+            $scope.$watch("search.info.data", function(newValue, oldValue) {
+                //if ($scope.name.length > 0) {
+                //    $scope.greeting = "Greetings " + $scope.name;
+                //}
+                console.log("newValue");
+                console.log(oldValue);
+                console.log(newValue);
+                if($scope.search.info.data.biological_object.efo_info[0][0].path){
+                    $scope.search.info.efo_path = $scope.search.info.data.biological_object.efo_info[0][0].path;
+                }
+            });
+
+            // will need a way of parsing filters too...
+            // $scope.parseFilters() ...
 
             // and fire the info search
             $scope.getInfo();
 
+            // get the data for the flower graph
+            $scope.getFlowerData();
+
             // then try get some data
-            $scope.getEvidence();
+            $scope.getRnaExpressionData();
+
+            // populate the tables
+            // RNA-expression table
+            //$('#rna-expression-table').ready(initTableRNA);
+            //setTimeout(initTableRNA, 1000); //I confess, this is a dirty hack to wait for the DOM... temporary though :)
+
         }
 
     }]).
