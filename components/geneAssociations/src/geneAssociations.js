afb00dd44bc53f0ce55d31b7d25344e77313900a
Miguel Pignatelli <emepyc@gmail.com>
2015-02-26 12:21:57 +0000

    unification of the bubbles tooltips. Now all show the evidence flower

--- components/geneAssociations/src/geneAssociations.js
+++ components/geneAssociations/src/geneAssociations.js
@@ -128,80 +128,34 @@ var geneAssociations = function () {
 	    }
 
 	    var obj = {};
-	    // Tooltip for the leaves
-	    if (node.is_leaf()) {
-		// tooltip is for a disease
-		var score = node.property("association_score");
-		obj.header = node.property("label") + " (Association Score: " + score + ")";
-		var loc = "#/gene-disease?t=" + config.target + "&d=" + node.property("efo_code");
-		obj.body="<div></div><a href=" + loc + ">View details</a>";
+	    var score = node.property("association_score");
+	    obj.header = node.property("label") + " (Association Score: " + score + ")";
+	    var loc = "#/gene-disease?t=" + config.target + "&d=" + node.property("efo_code");
+	    obj.body="<div></div><a href=" + loc + ">View details</a>";
 
-		var leafTooltip = tooltip.plain()
-		    .id(1)
-		    .width(180);
+	    var leafTooltip = tooltip.plain()
+		.id(1)
+		.width(180);
 
-		//Hijack of the fill callback
-		var tableFill = leafTooltip.fill();
+	    //Hijack of the fill callback
+	    var tableFill = leafTooltip.fill();
 
-		//Pass a new fill callback that calls the original one and decorates with flowers
-		leafTooltip.fill(function (data) {
-		    tableFill.call(this, data);
-		    var datatypes = node.property("datatypes");
-		    var flowerData = [
-			{"value":lookDatasource(datatypes, "genetic_association").score,  "label":"Genetics"},
-			{"value":lookDatasource(datatypes, "somatic_mutation").score,  "label":"Somatic"},
-			{"value":lookDatasource(datatypes, "known_drug").score,  "label":"Drugs"},
-			{"value":lookDatasource(datatypes, "rna_expression").score,  "label":"RNA"},
-			{"value":lookDatasource(datatypes, "affected_pathway").score,  "label":"Pathways"},
-			{"value":lookDatasource(datatypes, "animal_model").score,  "label":"Models"}
-		    ];
-		    flower.values(flowerData)(this.select("div").node());
-		});
-		
-		leafTooltip.call(this, obj);
-
-	    } else {
-		// tooltip is for a therapeutic area
-		obj.header = node.property("label");
-		obj.rows = [];
-		obj.rows.push({
-		    "label" : "Score",
-		    "value" : node.property("association_score")
-		});
-		// obj.rows.push({
-		//     "label" : "Evidence count",
-		//     "value" : Math.round(node.property("evidence_count"), 2)
-		// });
-
-		// if (node.property("focused") === 1) {
-		//     obj.rows.push({
-		// 	"label" : "Action",
-		// 	"value" : "Zoom Out",
-		// 	"obj" : node,
-		// 	"link" : function (node) {
-		// 	    node.property("focused", undefined);
-		// 	    bubblesView.focus(tree);
-		// 	}
-		//     });
-		// } else {
-		//     obj.rows.push({
-		// 	"label" : "Action",
-		// 	"value" : "Zoom In",
-		// 	"obj" : node,
-		// 	"link" : function (node) {
-		// 	    tree.apply( function (n) {
-		// 		n.property("focused", undefined);
-		// 	    });
-		// 	    node.property("focused", 1);
-		// 	    bubblesView.focus(node);
-		// 	}
-		//     });
-		// }
-		tooltip.table()
-	            .id(2)
-	            .width(180)
-	            .call (this, obj);
-	    }
+	    //Pass a new fill callback that calls the original one and decorates with flowers
+	    leafTooltip.fill(function (data) {
+		tableFill.call(this, data);
+		var datatypes = node.property("datatypes");
+		var flowerData = [
+		    {"value":lookDatasource(datatypes, "genetic_association").score,  "label":"Genetics"},
+		    {"value":lookDatasource(datatypes, "somatic_mutation").score,  "label":"Somatic"},
+		    {"value":lookDatasource(datatypes, "known_drug").score,  "label":"Drugs"},
+		    {"value":lookDatasource(datatypes, "rna_expression").score,  "label":"RNA"},
+		    {"value":lookDatasource(datatypes, "affected_pathway").score,  "label":"Pathways"},
+		    {"value":lookDatasource(datatypes, "animal_model").score,  "label":"Models"}
+		];
+		flower.values(flowerData)(this.select("div").node());
+	    });
+	    
+	    leafTooltip.call(this, obj);
 	};
 	bubblesView
 	    .onclick (bubble_tooltip);
