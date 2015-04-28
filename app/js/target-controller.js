8d38529ee3d68093ea0d1e9fd384d7c35a178694
Miguel Pignatelli <emepyc@gmail.com>
2015-04-28 15:34:19 +0100

    target-controller moved to a new file

---
+++ app/js/target-controller.js
@@ -0,0 +1,99 @@
+"use strict";
+
+angular.module('cttvControllers')
+
+/**
+ * TargetCtrl
+ * Controller for the target page
+ * It loads information about a given target
+ */
+    .controller ("TargetCtrl", ["$scope", "$location", "$log", "cttvAPIservice", function ($scope, $location, $log, cttvAPIservice)
+				{
+				    $log.log('TargetCtrl()');
+				    // var cttvRestApi = cttvApi();
+				    var geneId = $location.url().split("/")[2];
+				    // var url = cttvRestApi.url.gene({'gene_id' : geneId});
+				    // $log.log(url);
+				    // cttvRestApi.call(url)
+				    cttvAPIservice.getGene({
+					"gene_id": geneId
+				    })
+				        .then(function (resp) {
+					    resp = JSON.parse(resp.text);
+					    $scope.target = {
+						label : resp.approved_name || resp.ensembl_external_name,
+						symbol : resp.approved_symbol || resp.ensembl_external_name, //resp.approved_symbol || resp.approved_name || resp.ensembl_external_name,
+						id : resp.approved_id || resp.ensembl_gene_id,
+						description : resp.uniprot_function[0],
+						name : resp.approved_name || resp.ensembl_description
+					    };
+
+					    // Synonyms
+					    var syns = {};
+					    var synonyms = resp.symbol_synonyms;
+					    if (synonyms !== undefined) {
+						for (var i=0; i<synonyms.length; i++) {
+						    syns[synonyms[i]] = 1;
+						}
+					    }
+					    var prev_symbols = resp.previous_symbols;
+					    if (prev_symbols !== undefined) {
+						for (var j=0; j<prev_symbols.length; j++) {
+						    syns[prev_symbols[j]] = 1;
+						}
+					    }
+					    var name_synonyms = resp.name_synonyms;
+					    if (name_synonyms !== undefined) {
+						for (var j=0; j<name_synonyms.length; j++) {
+						    syns[name_synonyms[j]] = 1;
+						}
+					    }
+					    $log.log(synonyms);
+					    $scope.synonyms = _.keys(syns);
+
+					    // Uniprot
+					    $scope.uniprot = {
+						id : resp.uniprot_id,
+						subunits : resp.uniprot_subunit,
+						locations : resp.uniprot_subcellular_location,
+						accessions : resp.uniprot_accessions,
+						keywords : resp.uniprot_keywords
+					    }
+
+					    // Ensembl
+					    var isHuman = resp.ensembl_gene_id.substring(0,4) === "ENSG";
+					    $scope.ensembl = {
+						id : resp.ensembl_gene_id,
+						description : resp.ensembl_description,
+						isHuman : isHuman,
+						chr : resp.chromosome,
+						start : resp.gene_start,
+						end : resp.gene_end
+					    };
+
+					    // GO terms
+					    var goterms = _.filter(resp.dbxrefs, function (t) {return t.match(/^GO:/)});
+					    var cleanGoterms = _.map(goterms, function (t) {return t.substring(3, t.length)});
+					    var uniqGoterms = _.uniq(cleanGoterms);
+					    $scope.goterms = uniqGoterms;
+
+					    // Expression Atlas
+					    $scope.toggleBaselineExpression = function () {
+						$scope.eaTarget = resp.ensembl_gene_id;
+					    };
+					    $scope.toggleGenomeLocation = function () {
+						$scope.chr = resp.chromosome,
+						$scope.genomeBrowserGene = resp.ensembl_gene_id;
+					    }
+
+					    // Bibliography
+					    var bibliography = _.filter(resp.dbxrefs, function (t) {return t.match(/^PubMed/)});
+					    var cleanBibliography = _.map(bibliography, function (t) {return t.substring(7, t.length)});
+					    var bibliographyStr = cleanBibliography.join (",");
+					    $scope.pmids = bibliographyStr;
+					    $scope.pmidsLinks = (_.map(cleanBibliography,function (p) {return "EXT_ID:" + p})).join(" OR ");
+
+					    // Update the bindings
+					    // $scope.$apply();
+					});
+				}]);
