c7775bcaef883b699f5e97e58e8bac83c63e9657
Luca Fumis <lucaf@ebi.ac.uk>
2015-10-16 12:48:47 +0100

    Cherry-picking from 21ff71a1ae2bdea78d18b89787f91da287f4a81d

---
+++ app/js/disease-associations-controller.js
@@ -0,0 +1,167 @@
+
+
+/* Add to the cttv controllers module */
+angular.module('cttvControllers')
+
+/**
+ * AssociationsCtrl:
+ * Controller for the target associations page.
+ *
+ * It loads a list of associations for the given disease (efo).
+ * Any filters and facets are passed via the search part of the URL.
+ * On page load, if no filters are specified, we set the default datatypes (to filter out mouse data),
+ * or otherwise we just load the data.
+ * Filters modify the URL search:
+ * here we listen for changes to the URL and fire a new search when required.
+ * Then when we get the data, we update content and facets
+ */
+
+.controller ("diseaseAssociationsCtrl", ['$scope', '$location', '$log', 'cttvAPIservice', 'cttvFiltersService', function ($scope, $location, $log, cttvAPIservice, cttvFiltersService) {
+
+    'use strict';
+
+    $log.log('diseaseAssociationsCtrl()');
+
+
+
+    // ---------------------------
+    //  Initialiaze
+    // ---------------------------
+
+
+
+    // configure the "search" object
+    // to be exposed via scope
+    $scope.search = {
+        query : $location.path().split('/')[2],
+        label : "",
+        filename : "",
+        total : "..."
+    };
+
+
+    // reset the filters when loading a new page
+    // so we don't see the filters from the previous page...
+    cttvFiltersService.reset();
+
+    // Set page filters:
+    // this defines the order in which the facets are going to be displayed
+    cttvFiltersService.pageFacetsStack([
+        //cttvFiltersService.facetTypes.SCORE,        // adds a score facet to the page
+        cttvFiltersService.facetTypes.DATATYPES,    // adds a datatypes facet to the page
+        cttvFiltersService.facetTypes.PATHWAYS      // adds a pathways facet to the page
+    ]);
+
+
+
+    /*
+     * Get data to populate the table.
+     *
+     * @param filters: object of filtering categories, e.g. "datatypes"; each one is either a string or an array of strings
+     * Example:
+     * filters = {
+     *      datatypes: "known_drug",
+     *      pathway_type: [ "REACT_111102", "REACT_116125", "REACT_6900" ]
+     * }
+     * getData(filters);
+     */
+    var getData = function(filters){
+        $log.log("getData()");
+        var opts = {
+            disease: $scope.search.query,
+            datastructure: "flat",
+            expandefo: true
+        };
+        opts = cttvAPIservice.addFacetsOptions(filters, opts);
+
+        // TEST:
+        //cttvFiltersService.updateFacets(JSON.parse('{"data_distribution": {"buckets": {"0.0": {"value": 803},"0.2": {"value": 8},"0.4": {"value": 5},"0.8": {"value": 234},"0.6": {"value": 2}}}}'));
+
+
+
+        cttvAPIservice.getAssociations(opts).
+        then(
+            function(resp){
+                $log.log("Got new data...");
+
+                // 1: set the facets
+                // we must do this first, so we know which datatypes etc we actually have
+                $log.log(resp.body.status[0]);
+                $log.log(resp.body);
+                cttvFiltersService.updateFacets(resp.body.facets, undefined, resp.body.status);
+                // cttvFiltersService.status(resp.body.status);
+
+                // set the data
+                $scope.data = resp.body.data;
+                $scope.data.selected = {datatypes: cttvFiltersService.getSelectedFiltersRaw("datatypes")};
+
+                // set the total?
+                $scope.search.total = resp.body.data.length; //resp.body.total;
+            },
+            cttvAPIservice.defaultErrorHandler
+        );
+
+
+    };
+
+
+
+    // Set up a listener for the URL changes and
+    // when the search change, get new data
+    $scope.$on('$routeUpdate', function(){
+        $log.log("onRouteUpdate");
+        getData( cttvFiltersService.parseURL() );
+    });
+
+
+
+    // ---------------------------
+    //  Flow
+    // ---------------------------
+
+
+
+    // First off, get disease specific info to populate the top of the page
+    // This is independent of other data, so we just fire that here
+    cttvAPIservice.getDisease( {
+            code:$scope.search.query
+        } ).
+        then(
+            function(resp) {
+                $scope.search.label = resp.body.label;
+                $scope.search.filename = resp.body.label.split(" ").join("_");
+            },
+            cttvAPIservice.defaultErrorHandler
+        );
+
+
+
+    //
+    // Then onto the data
+    // No longer need to get unfiltered data first and all that
+    // We just get the data and display it, but:
+    //  1. Must set the default datatypes for this page
+    //  2. Get the data and facets
+    //  3. Listen for page changes
+
+    // Option 1: get the data without caring about filtered out mouse data
+    getData( cttvFiltersService.parseURL() );
+
+    // - OR -
+
+    // Option 2: set the default filterd-out mouse data on page load, in which case we get the data on URL change
+    /*if(_.keys(cttvFiltersService.parseURL()).length==0){
+        // NO SEARCH
+
+        // no search == no filters specified, so we set default filters: this will trigger a page reload
+        cttvFiltersService.setSelectedFilters({datatypes: cttvFiltersService.getDefaultSelectedDatatypes()});
+    } else {
+        // THERE IS A SEARCH
+
+        // make the call for data with the selected filters
+        getData( cttvFiltersService.parseURL() );
+    }*/
+
+
+
+}]);
---
+++ app/js/target-associations-controller.js
@@ -0,0 +1,304 @@
+
+/* Add to the cttv controllers module */
+angular.module('cttvControllers')
+
+/**
+ * AssociationsCtrl
+ * Controller for the target associations page
+ * It loads a list of associations for the given search
+ */
+    .controller('targetAssociationsCtrl', ['$scope', '$location', '$log', 'cttvUtils', 'cttvAPIservice', 'cttvFiltersService', 'cttvConsts', function ($scope, $location, $log, cttvUtils, cttvAPIservice, cttvFiltersService, cttvConsts) {
+        'use strict';
+
+	$log.log('targetAssociationsCtrl()');
+
+	var q = $location.path().split('/')[2];
+	$scope.search = {
+	    query : q
+	};
+
+    // reset the filters when loading a new page
+    // so we don't see the filters from the previous page...
+    cttvFiltersService.reset();
+
+    // Set filters
+    cttvFiltersService.pageFacetsStack([
+        //cttvFiltersService.facetTypes.SCORE,        // adds a score facet to the page
+        cttvFiltersService.facetTypes.DATATYPES
+    ]);
+
+    var filters = cttvFiltersService.parseURL();
+    var opts = cttvAPIservice.addFacetsOptions(filters, {});
+
+
+
+    // Set up a listener for the URL changes and
+    // when the search change, get new data
+    $scope.$on('$routeUpdate', function(){
+        $log.log("onRouteUpdate");
+        //$scope.filterDataTypes (cttvFiltersService.parseURL());
+        $scope.filter(cttvFiltersService.parseURL());
+    });
+
+
+
+    // get gene specific info
+    cttvAPIservice.getTarget( {
+            target_id:q
+        } ).
+        then(
+            function(resp) {
+                $scope.search.label = resp.body.approved_symbol;
+                $scope.search.filename = resp.body.approved_symbol.split(" ").join("_");
+            },
+            cttvAPIservice.defaultErrorHandler
+        );
+
+
+
+	$scope.loading = false;
+
+
+	$scope.toggleDataTypes = function () {
+	    $scope.toggleNavigation();
+	};
+
+
+    /**
+     * Parse the filters.
+     * That means it takes the list of filters as specified in the URL
+     */
+    $scope.filter = function(filters){
+        $log.log("filter()");
+
+        $scope.filters = filters;
+
+        // we parse for datatypes regardless of whether there are any,
+        // since the function there takes care of that case
+        parseDataTypes( filters );
+
+        $scope.score = {};
+    };
+
+    /*
+    $scope.filterDataTypes = function (filters) {
+        var currDatatypes = {};
+        //if (!Object.keys(filters).length) {
+        if( !filters[cttvConsts.DATATYPES] ){
+            for (var k=0; k<$scope.dataTypes.length; k++) {
+                currDatatypes[$scope.dataTypes[k].name] = $scope.dataTypes[k].label;
+            }
+        } else {
+            var filterDatatypes = filters[cttvConsts.DATATYPES];
+            for (var i=0; i<filterDatatypes.length; i++) {
+                var dt = filterDatatypes[i];
+                for (var j=0; j<$scope.dataTypes.length; j++) {
+                    var thisDT = $scope.dataTypes[j];
+                    if (thisDT.name === dt) {
+                        currDatatypes[thisDT.name] = thisDT.label;
+                        break;
+                    }
+                }
+            }
+        }
+        $scope.currentDataTypes = currDatatypes;
+    };*/
+
+
+    /*
+     * Private function; creates hash object for selected datatypes (or all) with corresponding labels for flower graph
+     */
+    var parseDataTypes = function (filters) {
+        var currDatatypes = {};
+
+        var filterDatatypes = filters[cttvConsts.DATATYPES] || cttvConsts.datatypesOrder.map(function(d){
+            return cttvConsts.datatypes[d];
+        });
+        filterDatatypes.forEach(function(dt){
+            currDatatypes[dt] = cttvConsts.datatypesLabels[dt.toUpperCase()];   // TODO: these labels should be in some sort of "aggregates", but at hte moment living in the consts service
+        });
+
+        $scope.currentDataTypes = currDatatypes;
+    };
+
+
+
+
+    //$scope.filterDataTypes(filters);
+    $scope.filter(filters);
+
+    $scope.updateFacets = function (facets) {
+        $log.log("**** updateFacets() ****");
+        cttvFiltersService.updateFacets(facets, "unique_disease_count");
+    };
+
+    /*
+    // trying to comment this out as it doesn't seem to be used
+	$scope.filterDataType = function (dataType) {
+	    var currentDataTypes = {};
+	    for (var i=0; i<$scope.dataTypes.length; i++) {
+		if ($scope.dataTypes[i].name === dataType.name) {
+		    $scope.dataTypes[i].selected = $scope.dataTypes[i].selected === false ? true : false;
+		}
+		if ($scope.dataTypes[i].selected) {
+		    var name = $scope.dataTypes[i].name;
+		    var label = $scope.dataTypes[i].label;
+		    currentDataTypes[name] = label;
+		}
+	    }
+	    $scope.currentDataTypes=currentDataTypes;
+	};*/
+
+
+	// This method filters out redundant diseases in different therapeutic areas (redundant diseases in the same therapeutic area is filtered out in the targetAssociation component (that controls the bubble view)
+	// It returns an array of the non-redundant diseases and another structure with the number of diseases per datatype
+	// TODO: Split the method in two: one returning each datastructre
+	$scope.nonRedundantDiseases = function (tas) {
+	    var diseasesInDatatypes = {};
+	    var nonRedundantDiseases = {};
+	    var filtered;
+	    for (var i=0; i<tas.length; i++) {
+		var ta = tas[i];
+		for (var j=0; j<ta.children.length; j++) {
+		    nonRedundantDiseases[ta.children[j].efo_code] = 1;
+		    var dts = ta.children[j].datatypes;
+		    for (var k=0; k<dts.length; k++) {
+			if (diseasesInDatatypes[dts[k].datatype] === undefined) {
+			    diseasesInDatatypes[dts[k].datatype] = 0;
+			}
+			diseasesInDatatypes[dts[k].datatype]++;
+		    }
+		}
+	    }
+	    return [nonRedundantDiseases, diseasesInDatatypes];
+	};
+
+
+	// This method sets the number of diseases supported by each datatype
+	// It needs to be called only once (on load) and without any filter applied
+	var allDiseases = [];
+	var includedDiseases = [];
+	$scope.setDiseasesInDatatypes = function () {
+        cttvAPIservice.getAssociations ({
+            target: $scope.search.query,
+            datastructure: "flat",
+            expandefo: true,
+	    })
+    		.then (function (resp) {
+                cttvFiltersService.updateFacets(resp.body.facets, "unique_disease_count");
+
+    		    // var data = resp.body.data;
+    		    // var dummy = geneAssociations()
+                //     .data(data);
+                //
+    		    // var ass = dummy.data().children || [];
+                // console.log(ass);
+                //
+    		    // var auxArr = $scope.nonRedundantDiseases(ass);
+    		    // allDiseases = _.keys(auxArr[0]);
+                // console.log(allDiseases);
+    		    // $scope.checkFilteredOutDiseases();
+
+    		    //var diseasesInDatatypes = auxArr[1];
+
+    		    // TODO: For now we are avoiding to show the number of diseases per datatype because this will cause inconsistencies
+    		    // with filtered out datatypes in the bubbles view. We will have to rethink this
+    		    // Get the diseases that are in all datatypes
+    		    // for (var n=0; n<$scope.dataTypes.length; n++) {
+    		        //$scope.dataTypes[n].diseases = diseasesInDatatypes[$scope.dataTypes[n].name] || 0;
+    		    // }
+    		},
+            cttvAPIservice.defaultErrorHandler
+        );
+	};
+	//$scope.setDiseasesInDatatypes();
+
+	// $scope.checkFilteredOutDiseases = function () {
+	//     // if (_.isEmpty(includedDiseases) || _.isEmpty(allDiseases)) {
+	//     // 	return
+	//     // }
+	//     var diff = _.difference(allDiseases, includedDiseases);
+	//     $scope.ndiseasesfiltered = diff.length;
+	//     $scope.diseasesFilteredMsg = $scope.ndiseasesfiltered ? " (" + $scope.ndiseasesfiltered + " diseases filtered out)" : "";
+	// };
+
+	$scope.setTherapeuticAreas = function (tas) {
+	    $scope.therapeuticAreas = tas;
+	    // var nonRedundantDiseases = $scope.nonRedundantDiseases(tas)[0];
+	    // $scope.ndiseases = _.keys(nonRedundantDiseases).length || 0;
+	    // includedDiseases = _.keys(nonRedundantDiseases);
+	    // $scope.checkFilteredOutDiseases();
+	};
+
+    $scope.n = {};
+    $scope.n.diseases = 0;
+    // $interval (function () {
+    //     console.log($scope.loading);
+    // }, 100);
+    // $scope.setTotalDiseases = function (diseases) {
+    //     $scope.ndiseases = diseases.length;
+    // };
+
+	// Therapeutic Areas Nav
+	$scope.focusEFO = "cttv_source";
+
+	$scope.tagroup = {};
+	$scope.tagroup.tas = {};
+	$scope.tagroup.filled = false;
+	$scope.tagroup.open = true;
+
+	var currentFocus = "cttv_disease";
+
+	$scope.selectTherapeuticArea = function (efo) {
+	    // Keep track of the state
+	    if (!$scope.tagroup.filled) {
+	    $scope.tagroup.filled = true;
+		for (var i=0; i<$scope.therapeuticAreas.length; i++) {
+		    var therapeuticArea = $scope.therapeuticAreas[i];
+		    $scope.tagroup.tas[therapeuticArea.name] = false;
+		}
+	    }
+	    if (efo === currentFocus) {
+	    	currentFocus = "cttv_disease";
+	    } else {
+	    	currentFocus = efo;
+	    }
+	    $scope.focusEFO = currentFocus;
+	};
+
+    // $scope.bubblesSelected = function () {
+    //     $(".cttv-nav").show();
+    // };
+    //
+    // $scope.treeSelected = function () {
+    //     $(".cttv-nav").show();
+    // };
+    //
+    // $scope.tableSelected = function () {
+    //     $(".cttv-nav").hide();
+    // }
+
+	$scope.toggleNavigation = function () {
+	    for (var ta in $scope.tagroup.tas) {
+		$scope.tagroup.tas[ta] = false;
+	    }
+	    $scope.focusEFO = "cttv_disease";
+	    currentFocus = "cttv_disease";
+	    $scope.diseasegroupOpen = false;
+	};
+
+    $scope.selectedDisease = 0;
+    $scope.selectDisease = function (d) {
+        $scope.highlightEFO = {efo: d.efo_code,
+            parent_efo: d._parent.efo_code,
+            datatypes: d.datatypes
+        };
+        $scope.selectedDisease++;
+        // if ($scope.selectedDisease === true) {
+        // 	$scope.selectedDisease = false;
+        // } else {
+        // 	$scope.selectedDisease = true;
+        // }
+    };
+
+}]);
