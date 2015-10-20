9b5d54666861c2c87f2625d7086e76c80d4ce211
Miguel Pignatelli <emepyc@gmail.com>
2015-10-20 10:26:18 +0100

    added some tests for the genome browser widget

--- protractor.conf.js
+++ protractor.conf.js
@@ -3,10 +3,9 @@ exports.config = {
 
     specs: [
 
-        'test/e2e/*.js'
-        'test/e2e/home.js',
-        'test/e2e/targetAssociation.js',
-        'test/e2e/evidence.js',
+        // 'test/e2e/home.js',
+        // 'test/e2e/targetAssociation.js',
+        // 'test/e2e/evidence.js',
         'test/e2e/targetProfile.js'
     ],
 
