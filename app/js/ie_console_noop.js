ea089ef418007a7b176f91e40e88d8936855200e
Miguel Pignatelli <emepyc@gmail.com>
2015-10-21 12:02:07 +0100

    added String.prototype.startsWith if not defined (IE)

--- app/js/ie_console_noop.js
+++ app/js/ie_console_noop.js
@@ -20,3 +20,12 @@
 	}
     }
 }());
+
+// Defined startsWith in IE
+if (!String.prototype.startsWith) {
+  String.prototype.startsWith = function(searchString, position) {
+      console.log("NEW STARTS WITH!");
+    position = position || 0;
+    return this.indexOf(searchString, position) === position;
+  };
+}
