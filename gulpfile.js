b3d223560941fe7d417ba36b088dda3380635d7d
Miguel Pignatelli <emepyc@gmail.com>
2015-10-23 17:07:15 +0100

    e2e tests moved to another repo

--- gulpfile.js
+++ gulpfile.js
@@ -5,7 +5,6 @@ var jshint = require('gulp-jshint');
 var mocha = require('gulp-mocha');
 var watch = require('gulp-watch');
 var karma = require("karma").server;
-var protractor = require("gulp-protractor").protractor;
 var uglify = require('gulp-uglify');
 var browserify = require('gulp-browserify');
 var sass = require('gulp-sass');
@@ -49,15 +48,6 @@ gulp.task('unitTest', function (done) {
     }, function() {
 	done();
     });
-});
-
-
-gulp.task('e2eTest', function () {
-    gulp.src(["./test/e2e/*.js"])
-        .pipe(protractor({
-	    configFile: "protractor.conf.js"
-	}))
-    .on('error', function(e) { throw e });
 });
 
 //gulp.task('test', ['unitTest', 'e2eTest']);
