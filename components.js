b0f8755ab1664d9457a8c5b57b9e636347b71dcf
Miguel Pignatelli <emepyc@gmail.com>
2016-04-06 12:48:29 +0100

    fixed the genome browser

--- components.js
+++ components.js
@@ -4,9 +4,10 @@ if (typeof tnt === "undefined") {
 tnt.board = require("tnt.genome");
 tnt.utils = require("tnt.utils");
 tnt.tooltip = require("tnt.tooltip");
-tnt.ensembl = require("tnt.ensembl");
+//tnt.ensembl = require("tnt.ensembl");
+tnt.rest = require("tnt.rest");
 
-var targetGenomeBrowser = require("cttv.targetGenomeBrowser");
+var targetGenomeBrowser = require("cttv.genome");
 // var bubblesView = require("cttv.bubblesView");
 // var geneAssociations = require("cttv.targetAssociations");
 var targetAssociations = require("cttv.targetAssociationsBubbles"); // new bubbles view
