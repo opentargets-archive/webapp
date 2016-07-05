00aa09b4c3c255c6e8fd040a44987c8035edd6b0
Miguel Pignatelli <emepyc@gmail.com>
2016-07-05 11:36:09 +0100

    pdb widgets working

--- app/plugins/pdb-directive.js
+++ app/plugins/pdb-directive.js
@@ -1,25 +1,34 @@
 angular.module('plugins')
-    .directive('pdbTarget', ['$log', '$http', function ($log, $http) {
+    .directive('pdbTarget', ['$log', '$http', '$compile', function ($log, $http, $compile) {
         'use strict';
 
         return {
             restrict: 'E',
-            templateUrl: "plugins/pdb.html",
+            // templateUrl: "plugins/pdb.html",
             scope: {
                 target: '='
             },
             link: function (scope, element, attrs) {
-                // TODO: This just takes a random PDB ID from the pdb section
-                // A more rationale way should be implemented
 
                 var uniprotId = scope.target.uniprot_id;
+                scope.pdbId = "4nfd";
+
+                // <div style="margin: 30px;position:relative;width:500px;height:500px;">
+                //     <pdb-topology-viewer entry-id="pdbId" entity-id="1"></pdb-topology-viewer>
+                // </div>
+                // <div style="margin: 30px;position:relative;width:500px;height:500px;">
+                //     <pdb-lite-mol pdb-id="pdbId" hide-controls="true"></pdb-lite-mol>
+                // </div>
+
 
                 $http.get("/proxy/www.ebi.ac.uk/pdbe/api/mappings/best_structures/" + uniprotId)
                     .then (function (resp) {
-                        console.log(resp);
                         var bestStructure = resp.data[uniprotId][0];
-                        console.log("BEST PROTEIN STRCTURE IS " + bestStructure.pdb_id);
                         scope.pdbId = bestStructure.pdb_id;
+
+                        var template = '<p>Showing the PDB structure for <a target=_blank href="http://www.ebi.ac.uk/pdbe/entry/pdb/' + bestStructure.pdb_id + '">' + bestStructure.pdb_id + '</a></p><tabset><tab heading=3D><div class="pdb-widget-container"><pdb-lite-mol pdb-id="pdbId" hide-controls=true></pdb-lite-mol></div></tab><tab heading=2D><div class="pdb-widget-container"><pdb-topology-viewer entry-id=' + bestStructure.pdb_id + ' entity-id=1></pdb-topology-viewer></div></tab><tabset>';
+                        var compiled = $compile(template)(scope);
+                        element.append(compiled);
                     });
 
 
