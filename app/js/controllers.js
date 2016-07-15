83a816eeb67b0b1dc31f53f5c265dbac1cada635
Luca Fumis <lucaf@ebi.ac.uk>
2016-07-15 11:15:31 +0100

    Added follow us to social media panel; this is now only enabled for the homepage

--- app/js/controllers.js
+++ app/js/controllers.js
@@ -41,4 +41,13 @@
         // expose the location;
         // note that exposing the page as $location.absUrl() does not work as that would not update when URL changes
         $scope.location = $location;
+        $scope.showSocialMedia = false;
+
+        // perhaps we should use our locationstate service instead?
+        $scope.$on('$locationChangeSuccess', function(){
+            // when the location is back to the homepage, we hide the social media icons and show the "follow us" instead...
+            if( $location.path()=='/' ){
+                $scope.showSocialMedia = false;
+            }
+        });
     }]);
