5236b38b79ce95992a250528431e44f6e20b2bc0
Luca Fumis <lucaf@ebi.ac.uk>
2016-08-09 19:33:22 +0100

    Temp change to the api configuration; will remove once RestApiJS component is deployed to npm

--- app/js/api-service.js
+++ app/js/api-service.js
@@ -49,6 +49,7 @@ angular.module('cttvServices')
 
         var api = cttvApi()
             .prefix("/api/")
+            .version("1.2")
             // .prefix("https://www.targetvalidation.org/api/")
             .appname("cttv-web-app")
             .secret("2J23T20O31UyepRj7754pEA2osMOYfFK")
