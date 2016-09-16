425d6fedf4c0b4e4a79559848060e889b9a2e817
Luca Fumis <lucaf@ebi.ac.uk>
2016-09-16 11:24:13 +0100

    Created directive for modal template and applied it to the export PNG directive/button

--- app/js/services.js
+++ app/js/services.js
@@ -279,3 +279,40 @@ angular.module('cttvServices', []).
     }])
 
 
+
+    .factory('cttvModal', ['$log', '$window', '$rootScope', '$uibModal', function($log, $window, $rootScope, $uibModal) {
+        var modalService = {};
+
+        /*modalService.modalInstance; // the single modal instance?
+        modalService.sayHello  = function(){$log.log("hello world")};
+        modalService.test = function(){
+            $log.log("modal test!");
+            $uibModal.open({
+                template: '<cttv-modal header="I am a test modal" on-ok="$uibModalInstance.sayHello()" hasok="true">'
+                              +'<p>And this is the content</p>'
+                          +'</cttv-modal>',
+                size: "sm",
+                // controller: ['$uibModalInstance',
+                //                 function($uibModalInstance){
+                //                     $log.log($uibModalInstance);
+                //                     return $uibModalInstance.dismiss(bob);
+                //             }]
+            });
+        }*/
+
+        // open; instance is stored as modalInstance so can always be retrieved (assuming)
+        /*modalService.open = function(o){
+            if(modalService.modalInstance){
+                modalService.modalInstance.close();
+            }
+            modalService.modalInstance = $uibModal.open(o);
+            return modalService.modalInstance;
+        }*/
+
+
+
+        return modalService;
+    }])
+
+
+
