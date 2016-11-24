39f2d2b8034da6a051fd4a3c755d10637fe1095f
deniseOme <denise@ebi.ac.uk>
2016-11-24 15:59:05 +0000

    Update gene-tree-directive.js

--- app/plugins/gene-tree-directive.js
+++ app/plugins/gene-tree-directive.js
@@ -5,9 +5,11 @@ angular.module('plugins')
         return {
             restrict: 'E',
             template: '<p class=cttv-section-intro>Phylogenetic tree showing the history of the human gene '
-            + '{{target.symbol}} based on protein sequences. The tree shows human paralogs and orthologs in'
-            + 'selected species. Click on any node to get more information about the homology relationship.'
-            + 'Check / Uncheck species to prune the tree accordingly</p>'
+            + '{{target.symbol}} based on protein sequences. All branches in this tree have '
+            + 'the same length (unscaled branches). You can also view the branches in ' 
+            + 'different lengths based on the number of evolutionary changes in the tree (select scaled branches) '
+            + 'All species are shown by default, but you can prune it to a subset of species by unticking the species accordingly. '
+            + 'Check Protein trees and orthologies for further details. </p>'
             + '<p ng-if=notFound==1>No gene tree has been found for {{target.symbol}}</p>'
             + '<png filename="{{target.approved_symbol}}-geneTree.png" track="geneTree"></png>',
 
