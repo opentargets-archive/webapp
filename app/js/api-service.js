f68aacbf179441447663d40c9af8a45f52ef6723
Miguel Pignatelli <emepyc@gmail.com>
2017-02-07 17:21:51 +0000

    removing reference to local rest api

--- app/js/api-service.js
+++ app/js/api-service.js
@@ -54,9 +54,9 @@ angular.module('cttvServices')
         };
 
         var api = cttvApi()
-            // .prefix("/api/")
-            .prefix('http://127.0.0.1:8123/api/')
-            .version("2.0")
+            .prefix("/api/")
+            // .prefix('https://127.0.0.1:8123/api/')
+            .version("latest")
             // .prefix("https://www.targetvalidation.org/api/")
             .appname("cttv-web-app")
             .secret("2J23T20O31UyepRj7754pEA2osMOYfFK")
