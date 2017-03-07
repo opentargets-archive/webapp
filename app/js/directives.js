2fb237708ed1fae8327a03e6db44694d792fce17
Luca Fumis <lucaf@ebi.ac.uk>
2017-03-07 11:44:13 +0000

    working version of picasso panel (search results page)

--- app/js/directives.js
+++ app/js/directives.js
@@ -1722,3 +1722,31 @@ angular.module('cttvDirectives', [])
             link: function(scope, element, attrs) {}
         };
     }])
+
+
+
+    /**
+     * Directive for the footer
+     * This is mostly so the footer loads like the other page content and not before it.
+     */
+    .directive('moreLessText', ['$log', function ($log) {
+        'use strict';
+
+        return {
+            restrict: 'EA',
+            scope: {
+                data: '=',
+                limit: '@'
+            },
+            template :  '<span>{{data | limitTo: limit }}</span>'
+                      + '<span ng-if="data.length>limit" ng-init="expd=false">'
+                      +     '<span ng-show="!expd" ng-click="expd=!expd"> &hellip; <a>[show more]</a></span>'
+                      +     '<span ng-show="expd">{{data | limitTo: data.length:limit }}<span ng-click="expd=!expd"> <a>[show less]</a></span></span>'
+                      + '</span>',
+            link: function(scope, element, attrs) {
+                $log.log(scope);
+            }
+        };
+    }])
+
+
