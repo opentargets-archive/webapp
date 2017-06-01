d2b343a94ffd0e393ef99d07d7cb5c65733c66ec
Priyanka Wankar <priyanka.wankar@biogen.com>
2017-06-01 17:53:53 -0400

    Testing phewas data as part of common diseases

--- app/js/config-service.js
+++ app/js/config-service.js
@@ -23,8 +23,8 @@ angular.module('cttvServices').
             // if multiple sources are needed (e.g. for somatic mutation table), specify these in an array
             evidence_sources : {
                 genetic_association : {
-                    common : [dbs.GWAS],
-                    rare : [dbs.UNIPROT, dbs.EVA, dbs.UNIPROT_LITERATURE, dbs.GENE_2_PHENOTYPE, dbs.PHEWAS]
+                    common : [dbs.GWAS, dbs.PHEWAS],
+                    rare : [dbs.UNIPROT, dbs.EVA, dbs.UNIPROT_LITERATURE, dbs.GENE_2_PHENOTYPE]
                 },
                 somatic_mutation : [dbs.CANCER_GENE_CENSUS, dbs.EVA_SOMATIC, dbs.INTOGEN],
                 known_drug : [dbs.CHEMBL],
