a87d8fd06c325d810e3354e8f7f40f9089ae932d
LucaFumis <luca.fumis@gmail.com>
2017-07-08 12:36:16 +0100

    Added separate markdown API documentation (#151)
    * APIHOST env var controls api to use
    
    * attempt to per-branch api via netlify contexts
    
    * setting a different APIHOST in netlify.toml
    
    * Added separate markdown API documentation
    
    * pointing to appengine api
    
    * pointing to a slightly different swagger place
    
    * make address consistent with google cloud endpts
    
    * point everything to appengine deployment and add lots of warnings
    
    * try one less slash
    
    * Added global config, trying to pass api path
    
    * removed redundant /
    
    * test: swagger url passed without quotation
    
    * Modifed 3rd party swagger directive to accept static string

--- app/js/directives.js
+++ app/js/directives.js
@@ -1220,3 +1220,30 @@ angular.module('cttvDirectives', [])
     }])
 
 
+
+    /**
+     * Directive to parse Markdown documents
+     */
+    .directive('mdParser', ['$log','$http', '$sce', function ($log, $http, $sce) {
+        'use strict';
+
+        return {
+            restrict: 'EA',
+            scope: {
+                url: '@'     // the url of the resource
+            },
+            template : '<div ng-bind-html="md"></div>',
+            link: function(scope, element, attrs) {
+                $http.get(scope.url)
+                .then(function successCallback(response) {
+                    // this callback will be called asynchronously
+                    // when the response is available
+                    scope.md = $sce.trustAsHtml( marked(response.data) );
+                }, function errorCallback(response) {
+                    // called asynchronously if an error occurs
+                    // or server returns response with an error status.
+                    $log.log(response)
+                });
+            }
+        };
+    }])
