aafb9c2c69208b37ccaa86cfaab1fd5049d4cc54
Luca Fumis <lucaf@ebi.ac.uk>
2017-07-19 15:03:53 +0100

    testing call to Ghost API

--- app/js/directives.js
+++ app/js/directives.js
@@ -1149,6 +1149,98 @@ angular.module('cttvDirectives', [])
 
 
 
+    .directive('otBlogFeed', ['$log', '$http', function ($log, $http) {
+        'use strict';
+
+        return {
+            restrict: 'EA',
+            scope: {},
+            template :   '<div class="hp-blog-feed">'
+                        //+'    <p>{{feed.title}}</p><p>{{feed.description}}</p>'
+                        +'    <div class="hp-blog-feed-post" ng-repeat="post in feed.item">'
+                        +'        <h5 class="hp-blog-feed-post-header"><a href="{{post.link}}">'
+                        +'            {{post.title}}'
+                        +'        </a></h5>'
+                        +'        <div class="clearfix text-lowlight">'
+                        +'            <p class="pull-left">By {{post.creator.toString()}}</p>'           // author
+                        //+'            <p class="pull-right">{{post.pubDate.toLocaleDateString("en-GB")}}</p>' // date
+                        +'            <p class="pull-right">{{post.pubDate.getDate()}} {{post.pubDate.getMonth() | monthToString}} {{post.pubDate.getFullYear()}}</p>' // date
+                        +'        </div>'
+                        +'        <div ng-bind-html="post.description | stripTags | ellipseText:130"></div>'                            // long description
+                        +'        <div class="text-lowlight text-small" ng-if="post.category"><span class="fa fa-tags"></span> {{post.category.join(", ")}}</div>'   // tags
+                        +'    </div>'
+                        +'</div>',
+            link: function(scope, element, attrs) {
+                /*$http.get('/proxy/blog.opentargets.org/rss/')
+                //$http.get('rss.xml')    // JUST FOR TESTING and DEVELOPING LOCALLY WITHOUT THE PROXY
+                    .then(function successCallback(response) {
+
+                        var x2js = new X2JS();
+                        var feed = x2js.xml_str2json(response.data);
+                        // $log.log(feed);
+
+                        // The feed should be already ordered by date, but it seems sometimes it isn't,
+                        // so for now we sort it; maybe in the future we won't need to... will ask Eliseo about blog pub dates
+                        // 1. parse the pub dates to unix timestamp
+                        feed.rss.channel.item.forEach(function(i){
+                            i.pubDate = new Date(i.pubDate);
+                        });
+                        // 2. sort item array by timestamp
+                        feed.rss.channel.item.sort(function (a, b) {
+                            return b.pubDate.getTime() - a.pubDate.getTime();
+                        });
+
+
+                        scope.feed = feed.rss.channel;
+
+                    }, function errorCallback(response) {
+                        // called asynchronously if an error occurs
+                        // or server returns response with an error status.
+                        $log.log(response);
+                    });*/
+
+                var url = ghost.url.api('tags', {
+                            limit: 'all',
+                            include: 'count.posts',
+                            order: 'count.posts DESC'
+                          });
+                $log.log("calling...");
+                //$http.get('https://proxy.targetvalidation.org/blog.opentargets.org/ghost/api/v0.1/tags/?limit=all&include=count.posts&order=count.posts%20DESC&client_id=ghost-frontend&client_secret=a9fe83f50655')
+                $http.get('/proxy/blog.opentargets.org/ghost/api/v0.1/tags/?limit=all&include=count.posts&order=count.posts%20DESC&client_id=ghost-frontend&client_secret=a9fe83f50655')
+                    .then(function successCallback(response) {
+
+                        $log.log("*** ghost get: ");
+                        $log.log(response);
+
+                        // The feed should be already ordered by date, but it seems sometimes it isn't,
+                        // so for now we sort it; maybe in the future we won't need to... will ask Eliseo about blog pub dates
+                        // 1. parse the pub dates to unix timestamp
+
+                        // feed.rss.channel.item.forEach(function(i){
+                        //     i.pubDate = new Date(i.pubDate);
+                        // });
+
+                        // 2. sort item array by timestamp
+                        // feed.rss.channel.item.sort(function (a, b) {
+                        //     return b.pubDate.getTime() - a.pubDate.getTime();
+                        // });
+
+
+                        scope.feed = []; //feed.rss.channel;
+
+                    }, function errorCallback(response) {
+                        // called asynchronously if an error occurs
+                        // or server returns response with an error status.
+                        $log.log("error");
+                        $log.log(response);
+                    });
+
+            }
+        };
+    }])
+
+
+
     /**
      * The searchbox with search suggestions
      */
