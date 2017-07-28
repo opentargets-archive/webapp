a1beb88a786b98c6e54ef710957a66402584ef21
Eliseo Papa <elipapa@users.noreply.github.com>
2017-07-28 14:19:24 +0100

    testing gulp install

--- docker-entrypoint.sh
+++ docker-entrypoint.sh
@@ -10,6 +10,9 @@ export ENSEMBL_API_KEY=${ENSEMBL_API_KEY:=YOUR_KEY_HERE}
 envsubst < /etc/nginx/conf.d/app_server.template > /etc/nginx/conf.d/app_server.conf
 envsubst '$REST_API_SCHEME:$ENSEMBL_API_KEY' < /etc/nginx/conf.d/rest_api_scheme.template > /etc/nginx/conf.d/rest_api_scheme.conf
 
+# read custom.json and merge it to default.json at _runtime_
+gulp build-config
+
 echo "======================================="
 echo "POINTING TO REST API"
 echo at ${REST_API_SCHEME}://${REST_API_SERVER}
