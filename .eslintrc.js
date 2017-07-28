c8bc7bd3ca42d4811f99c3eaba4a7c72a580fc9f
peatroot <garethpeat@gmail.com>
2017-07-28 16:11:39 +0100

    Add angular-plugin-compat to run checks against caniuse db

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,10 +1,14 @@
 module.exports = {
+    "parser": "babel-eslint",
     "env": {
         "browser": true
     },
     "extends": [
         "eslint:recommended",
         "plugin:angular/johnpapa"
+    ],
+    "plugins": [
+        "compat"
     ],
     "rules": {
         "indent": [
@@ -24,12 +28,16 @@ module.exports = {
             "always"
         ],
         /**
-         * Angular Rules
+         * Angular rules
          * Individual rules can be disabled, which we may wish to
          * enable at some point. For example, controllers named
          * eg. *Ctrl.js will throw an error, so the following rule
          * could be switched off (0).
-         */ 
+         */
         // "angular/controller-name": 0
+        /**
+         * Compatibility rules
+         */
+        "compat/compat": 2
     }
 };
