f80ad0cafcc737d6e171cdfc0d94d3041b38bdd8
peatroot <garethpeat@gmail.com>
2017-08-29 11:26:40 +0100

    Rename NotifyCtrl to NotifyController

--- app/src/components/masthead/masthead-notifications-menu-directive.js
+++ app/src/components/masthead/masthead-notifications-menu-directive.js
@@ -10,7 +10,7 @@ angular.module('otDirectives')
             scope: {},
 
             template: ''
-                        + '<div ng-cloak class="notification" ng-show="notificationsLeft" ng-controller="NotifyCtrl">'
+                        + '<div ng-cloak class="notification" ng-show="notificationsLeft" ng-controller="NotifyController">'
                         + '     <div class="counter" ng-bind-html="notificationsLeft"></div>'
                         + '     <i ng-click="notify()" class="fa fa-bell" aria-hidden="true"></i>'
                         + '</div>'
