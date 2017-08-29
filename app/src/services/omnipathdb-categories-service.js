8b10507a431afecad38fb99b94f08ffac31f8503
peatroot <garethpeat@gmail.com>
2017-08-29 09:35:13 +0100

    Autofix linting

--- app/src/services/omnipathdb-categories-service.js
+++ app/src/services/omnipathdb-categories-service.js
@@ -1,25 +1,25 @@
 angular.module('cttvServices')
-.factory('otOmnipathdbCategories', [function () {
-  return {
-      'Pathways': {
-          'SignaLink3': true,
-          'Signor': true,
-          'Reactome': true,
-          'SPIKE': true
-      },
-      'Enzyme-substrate': {
-          'PhosphoPoint': true,
-          'HPRD': true,
-          'HPRD-phos': true,
-          'MIMP': true,
-          'HuPho': true
-      },
-      'PPI': {
-          'BioGRID': true,
-          'InnateDB': true,
-          'IntAct': true,
-          'DIP': true,
-          'STRING': true
-      }
-  };
-}]);
+    .factory('otOmnipathdbCategories', [function () {
+        return {
+            'Pathways': {
+                'SignaLink3': true,
+                'Signor': true,
+                'Reactome': true,
+                'SPIKE': true
+            },
+            'Enzyme-substrate': {
+                'PhosphoPoint': true,
+                'HPRD': true,
+                'HPRD-phos': true,
+                'MIMP': true,
+                'HuPho': true
+            },
+            'PPI': {
+                'BioGRID': true,
+                'InnateDB': true,
+                'IntAct': true,
+                'DIP': true,
+                'STRING': true
+            }
+        };
+    }]);
