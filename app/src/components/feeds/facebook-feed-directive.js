ca9871a9f41edff765b783b000c6311c5bf84df1
peatroot <garethpeat@gmail.com>
2017-08-29 16:18:19 +0100

    Split out template

--- app/src/components/feeds/facebook-feed-directive.js
+++ app/src/components/feeds/facebook-feed-directive.js
@@ -5,16 +5,7 @@ angular.module('otDirectives')
         return {
             restrict: 'AE',
             scope: {},
-            template: '<div class="fb-page"'
-                        + '    data-href="https://www.facebook.com/OpenTargets/"'
-                        + '    data-tabs="timeline"'
-                        + '    data-small-header="true"'
-                        + '    data-adapt-container-width="true"'
-                        + '    data-hide-cover="false"'
-                        + '    data-show-facepile="false"'
-                        + '    height="400">'
-                        + '    <blockquote cite="https://www.facebook.com/OpenTargets/" class="fb-xfbml-parse-ignore"><a href="https://www.facebook.com/OpenTargets/">Open Targets</a></blockquote>'
-                        + '</div>',
+            templateUrl: 'src/components/feeds/facebook-feed.html',
             link: function () {
                 try {
                     FB.XFBML.parse();
