617a98420e687d39bb87c9037c4ef347fc811275
Priyanka Wankar <priyanka.wankar@biogen.com>
2017-08-31 22:12:19 -0400

    Adding divider logic in template

--- app/js/nferx-table-directive.js
+++ app/js/nferx-table-directive.js
@@ -153,20 +153,20 @@ angular.module('cttvDirectives')
 
             return {
                 restrict: 'EA',
-                //templateUrl: 'partials/nferx-table.html',
+                templateUrl: 'partials/nferx-table.html',
                 //template : ' <div ng-show="{{total}}>0" style="width: 100%; height: 1px; background: #CCC; margin-top:30px; margin-bottom:30px;"></div>'
-                template: '<div class="table-panel"> <table class="table cttv-evidence-table" id="literature3-table">
-                + '<thead>'+ '<tr>' + '<th>Access level</th>'
-            +'<th>Disease</th>'
-            +'<th>Cosine Distance</th>'
-            +'<th>Nferx URL</th>'
-        +'</tr>'
-        +'</thead>'
-
-        +'<tbody>'
-        +'</tbody>'
-    +'</table>'
-+'</div>',
+//                template: '<div class="table-panel"> <table class="table cttv-evidence-table" id="literature3-table">
+//                + '<thead>'+ '<tr>' + '<th>Access level</th>'
+//            +'<th>Disease</th>'
+//            +'<th>Cosine Distance</th>'
+//            +'<th>Nferx URL</th>'
+//        +'</tr>'
+//        +'</thead>'
+//
+//        +'<tbody>'
+//        +'</tbody>'
+//    +'</table>'
+//+'</div>',
                 scope: {
                     total: '='
                     target: '=',
