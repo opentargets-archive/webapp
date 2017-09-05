14b4c1fe2293280b262d5f83fde8819363a4fc34
peatroot <garethpeat@gmail.com>
2017-09-05 16:56:47 +0100

    Autofix eslint errors

--- app/src/services/flat-facet-parser-generator.js
+++ app/src/services/flat-facet-parser-generator.js
@@ -1,8 +1,7 @@
 angular.module('facets')
-.factory('flatFacetParserGenerator', ['$log', 'cttvFilterTypesService', function($log, cttvFilterTypesService) {
-  
-  var generate = function (useKeyAsLabel) {
-    /**
+    .factory('flatFacetParserGenerator', ['$log', 'cttvFilterTypesService', function ($log, cttvFilterTypesService) {
+        var generate = function (useKeyAsLabel) {
+            /**
      * Parse function.
      * 
      * Each FacetParser service MUST expose a parse function. This function
@@ -19,91 +18,89 @@ angular.module('facets')
      * @param {object} apiData - The data object for this facet (from the API)
      * @param {object} facetsGlobal - A global object (exposing methods for eg. updating all facets)
      */
-    var parse = function (facetName, apiData, facetsGlobal, countsKey, options) {
-
-      /**
+            var parse = function (facetName, apiData, facetsGlobal, countsKey, options) {
+                /**
        * Create an array of boolean filters (each of which provides the needed
        * state and methods for a checkbox)
        */
-      var filters = apiData[facetName].buckets.map(function (bucket) {
-        return new cttvFilterTypesService.BooleanFilter({
-          key: bucket.key,
-          label: useKeyAsLabel ? bucket.key : bucket.label,
-          count: bucket[countsKey].value,
-          // TODO: Fix enabled, checked
-          enabled: true,
-          checked: false
-        }, facetsGlobal);
-      });
+                var filters = apiData[facetName].buckets.map(function (bucket) {
+                    return new cttvFilterTypesService.BooleanFilter({
+                        key: bucket.key,
+                        label: useKeyAsLabel ? bucket.key : bucket.label,
+                        count: bucket[countsKey].value,
+                        // TODO: Fix enabled, checked
+                        enabled: true,
+                        checked: false
+                    }, facetsGlobal);
+                });
 
-      /**
+                /**
        * Toggle method for all filters
        */
-      var setAllChecked = function (value) {
-        // update state
-        filters.forEach(function (filter) {
-          filter.setChecked(value);
-        });
-        // global update
-        facetsGlobal.update();
-      };
+                var setAllChecked = function (value) {
+                    // update state
+                    filters.forEach(function (filter) {
+                        filter.setChecked(value);
+                    });
+                    // global update
+                    facetsGlobal.update();
+                };
 
-      /**
+                /**
        * Serialize this facet for the url state.
        * @param {object} urlObj - The URL object. This object can be mutated and must
        *                          then be returned.
        */
-      var serialize = function (urlObj) {
-        urlObj[facetName] = filters.filter(function (filter) {
-          return filter.checked;
-        }).map(function (filter) {
-          return filter.key;
-        });
-        return urlObj;
-      };
+                var serialize = function (urlObj) {
+                    urlObj[facetName] = filters.filter(function (filter) {
+                        return filter.checked;
+                    }).map(function (filter) {
+                        return filter.key;
+                    });
+                    return urlObj;
+                };
 
-      /**
+                /**
        * Check if a boolean filter is checked according to the URL object.
        * @param {object} urlObj - Object representation of the URL
        * @param {string} key - Key of the filter to check
        */
-      var filterIsChecked = function (urlObj, key) {
-        return (urlObj &&
+                var filterIsChecked = function (urlObj, key) {
+                    return (urlObj &&
                 urlObj[facetName] &&
                 (urlObj[facetName] === key ||
                  urlObj[facetName].indexOf(key) >= 0));
-      };
+                };
 
-      /**
+                /**
        * Deserialize the url state and update the facet state.
        * @param {object} urlObj - The URL object
        */
-      var deserialize = function (urlObj) {
-        filters.forEach(function (filter) {
-          filter.checked = filterIsChecked(urlObj, filter.key);
-        })
-      };
+                var deserialize = function (urlObj) {
+                    filters.forEach(function (filter) {
+                        filter.checked = filterIsChecked(urlObj, filter.key);
+                    });
+                };
 
-      // Load the url state to update checked status etc.
-      deserialize(facetsGlobal.getUrlObject());
+                // Load the url state to update checked status etc.
+                deserialize(facetsGlobal.getUrlObject());
 
-      // Return the Facet object
-      return {
-        filters: filters,
-        setAllChecked: setAllChecked,
-        serialize: serialize,
-        deserialize: deserialize,
-        options: options
-      };
-    }
+                // Return the Facet object
+                return {
+                    filters: filters,
+                    setAllChecked: setAllChecked,
+                    serialize: serialize,
+                    deserialize: deserialize,
+                    options: options
+                };
+            };
 
-    return {
-      parse: parse,
-    };
-  }
-  
-  return {
-    generate: generate
-  }
+            return {
+                parse: parse
+            };
+        };
 
-}]);
+        return {
+            generate: generate
+        };
+    }]);
--- app/src/services/nested-facet-parser-generator.js
+++ app/src/services/nested-facet-parser-generator.js
@@ -1,27 +1,25 @@
 angular.module('facets')
-.factory('nestedFacetParserGenerator', ['$log', 'cttvFilterTypesService', function($log, cttvFilterTypesService) {
-  
-  var generate = function (accessor) {
-    var parse = function (facetName, apiData, facetsGlobal, countsKey, options) {
+    .factory('nestedFacetParserGenerator', ['$log', 'cttvFilterTypesService', function ($log, cttvFilterTypesService) {
+        var generate = function (accessor) {
+            var parse = function (facetName, apiData, facetsGlobal, countsKey, options) {
+                var flatFilters = [];
+                var nestedFilters;
 
-      var flatFilters = [];
-      var nestedFilters;
-
-      /**
+                /**
        * getExistingFilter
        * Check if a filter with a specific key has already been created
        * and, if so, retrieve it.
        * @param {string} key - The key of the filter to search for
        * @param {array} flatFilters - The (flat) array of filters to look in
        */
-      var getExistingFilter = function (key, flatFilters) {
-        var match = flatFilters.filter(function (filter) {
-          return filter.key === key;
-        });
-        return match.length > 0 ? match[0] : null;
-      };
+                var getExistingFilter = function (key, flatFilters) {
+                    var match = flatFilters.filter(function (filter) {
+                        return filter.key === key;
+                    });
+                    return match.length > 0 ? match[0] : null;
+                };
 
-      /**
+                /**
        * constructFilters
        * Parse the API data and recursively build a nested filter structure.
        * @param {object} data - The API data
@@ -34,126 +32,125 @@ angular.module('facets')
        * the filters from the apiData, the strategy is to keep a flat
        * list of filters, which the nested filters reference.
        */
-      var constructFilters = function (data, flatFilters, parent) {
-        // iterate the buckets, checking for existance in the flat list
-        // and constructing a new Filter if there is none
-        return data.buckets.map(function (bucket) {
-          // get the existing filter, if it exists
-          var filter = getExistingFilter(bucket.key, flatFilters);
-          // if there is no existing filter, create one
-          if (!filter) {
-            filter = new cttvFilterTypesService.NestedBooleanFilter({
-              key: bucket.key,
-              label: bucket.label,
-              count: bucket[countsKey].value,
-              enabled: true,
-              checked: false,
-              children: null,
-              facetName: facetName,
-            }, facetsGlobal);
-            // add to the flat list
-            flatFilters.push(filter);
-          }
-          // track the parent (if any)
-          if (parent) {
-            filter.addParent(parent);
-          }
-          // construct the filter's children
-          var children = null;
-          if (bucket[accessor]) {
-            children = constructFilters(bucket[accessor], flatFilters, filter);
-          }
-          filter.children = children;
-          // return the created/updated filter
-          return filter;
-        });
-      }
+                var constructFilters = function (data, flatFilters, parent) {
+                    // iterate the buckets, checking for existance in the flat list
+                    // and constructing a new Filter if there is none
+                    return data.buckets.map(function (bucket) {
+                        // get the existing filter, if it exists
+                        var filter = getExistingFilter(bucket.key, flatFilters);
+                        // if there is no existing filter, create one
+                        if (!filter) {
+                            filter = new cttvFilterTypesService.NestedBooleanFilter({
+                                key: bucket.key,
+                                label: bucket.label,
+                                count: bucket[countsKey].value,
+                                enabled: true,
+                                checked: false,
+                                children: null,
+                                facetName: facetName
+                            }, facetsGlobal);
+                            // add to the flat list
+                            flatFilters.push(filter);
+                        }
+                        // track the parent (if any)
+                        if (parent) {
+                            filter.addParent(parent);
+                        }
+                        // construct the filter's children
+                        var children = null;
+                        if (bucket[accessor]) {
+                            children = constructFilters(bucket[accessor], flatFilters, filter);
+                        }
+                        filter.children = children;
+                        // return the created/updated filter
+                        return filter;
+                    });
+                };
 
-      /**
+                /**
        * Toggle method for all filters
        */
-      var setAllChecked = function (value) {
-        // update state
-        nestedFilters.forEach(function (filter) {
-          filter.setChecked(value);
-        });
-        // global update
-        facetsGlobal.update();
-      };
+                var setAllChecked = function (value) {
+                    // update state
+                    nestedFilters.forEach(function (filter) {
+                        filter.setChecked(value);
+                    });
+                    // global update
+                    facetsGlobal.update();
+                };
 
-      /**
+                /**
        * Serialize this facet for the url state.
        * @param {object} urlObj - The URL object. This object can be mutated and must
        *                          then be returned.
        */
-      var serialize = function (urlObj) {
-        urlObj[facetName] = flatFilters.filter(function (filter) {
-          return filter.checked;
-        }).map(function (filter) {
-          return filter.key;
-        });
-        return urlObj;
-      };
+                var serialize = function (urlObj) {
+                    urlObj[facetName] = flatFilters.filter(function (filter) {
+                        return filter.checked;
+                    }).map(function (filter) {
+                        return filter.key;
+                    });
+                    return urlObj;
+                };
 
-      /**
+                /**
        * Initialize
        */
-      var init = function () {
-        // setup the nested filters from the api data (structure)
-        nestedFilters = constructFilters(apiData[facetName], flatFilters, null);
+                var init = function () {
+                    // setup the nested filters from the api data (structure)
+                    nestedFilters = constructFilters(apiData[facetName], flatFilters, null);
 
-        // load the url state (update checked statuses etc.)
-        deserialize(facetsGlobal.getUrlObject());
-      }
+                    // load the url state (update checked statuses etc.)
+                    deserialize(facetsGlobal.getUrlObject());
+                };
 
-        /**
+                /**
        * Check if a boolean filter is checked according to the URL object.
        * Note that the key is an number, but the urlObj may be a string (
        * ie. 6 vs '6')
        * @param {object} urlObj - Object representation of the URL
        * @param {string} key - Key of the filter to check
        */
-      var filterIsChecked = function (urlObj, key) {
-      return (urlObj &&
+                var filterIsChecked = function (urlObj, key) {
+                    return (urlObj &&
               urlObj[facetName] &&
               (urlObj[facetName] === ('' + key) ||
                urlObj[facetName].indexOf('' + key) >= 0));
-      };
+                };
 
-      /**
+                /**
        * Deserialize the url state and update the facet state.
        * @param {object} urlObj - The URL object
        */
-      var deserialize = function (urlObj) {
-        nestedFilters.forEach(function (filter) {
-          filter.setChecked(filterIsChecked(urlObj, filter.key));
-          if (filter.children) {
-            filter.children.forEach(function (childFilter) {
-              childFilter.setChecked(filterIsChecked(urlObj, childFilter.key));
-            });
-          }
-        });      
-      };
+                var deserialize = function (urlObj) {
+                    nestedFilters.forEach(function (filter) {
+                        filter.setChecked(filterIsChecked(urlObj, filter.key));
+                        if (filter.children) {
+                            filter.children.forEach(function (childFilter) {
+                                childFilter.setChecked(filterIsChecked(urlObj, childFilter.key));
+                            });
+                        }
+                    });
+                };
 
-      init();    
+                init();
 
-      // Return the Facet object
-      return {
-        filters: nestedFilters,
-        serialize: serialize,
-        deserialize: deserialize,
-        setAllChecked: setAllChecked,
-        options: options
-      };
-    }
+                // Return the Facet object
+                return {
+                    filters: nestedFilters,
+                    serialize: serialize,
+                    deserialize: deserialize,
+                    setAllChecked: setAllChecked,
+                    options: options
+                };
+            };
 
-    return {
-      parse: parse
-    };
-  }
-  
-  return {
-    generate: generate
-  }
+            return {
+                parse: parse
+            };
+        };
 
-}]);
+        return {
+            generate: generate
+        };
+    }]);
