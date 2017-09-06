13fa9b4c1d8937a5512dc593253dfdce77e9ae68
peatroot <garethpeat@gmail.com>
2017-09-06 09:53:17 +0100

    Update target-class facet

--- app/facets/target-class/target-class-facet.js
+++ app/facets/target-class/target-class-facet.js
@@ -1,16 +1,10 @@
 angular.module('facets')
-
-    /**
-     * The Target class facet
-     */
-    .directive('targetClassFacet', [function () {
-        'use strict';
-
-        return {
-            restrict: 'AE',
-            scope: {
-                facet: '='
-            },
-            templateUrl: 'directives/generic-nested-facetcollection.html'
-        };
-    }]);
+.directive('targetClassFacet', [function () {
+  return {
+    restrict: 'E',
+    scope: {
+      facet: '='
+    },
+    templateUrl: 'facets/target-class/target-class-facet.html',
+  };
+}]);
