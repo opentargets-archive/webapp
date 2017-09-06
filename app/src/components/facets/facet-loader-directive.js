66055fc0446e20f5c96940b72d8cb3d464f950f1
peatroot <garethpeat@gmail.com>
2017-09-06 11:28:21 +0100

    Add ot prefix to facet-loader directive

--- app/src/components/facets/facet-loader-directive.js
+++ app/src/components/facets/facet-loader-directive.js
@@ -4,23 +4,19 @@ angular.module('otFacets')
     * Top level container for all the facets.
     * This contains accordion etc
     */
-    .directive('facetLoader', ['$compile', function ($compile) {
+    .directive('otFacetLoader', ['$compile', function ($compile) {
         'use strict';
 
         return {
-
             restrict: 'AE',
-
             scope: {
                 'plugin': '@',
                 'facet': '='
             },
-
             link: function (scope, elem) {
                 var template = '<' + scope.plugin + ' facet=facet></' + scope.plugin + '>';
                 var compiled = $compile(template)(scope);
                 elem.append(compiled);
             }
-
         };
     }]);
