8f8d5dd066231dc9d10d21312d91fce2192430e6
peatroot <garethpeat@gmail.com>
2017-09-06 11:39:07 +0100

    Update facets-directive

--- app/src/components/facets/facets-directive.js
+++ app/src/components/facets/facets-directive.js
@@ -3,28 +3,15 @@ angular.module('otFacets')
     * Top level container for all the facets.
     * This contains accordion etc
     */
-    .directive('otFacets', ['otFacetsFilters', function (otFacetsFilters) {
+    .directive('otFacets', ['otFacetsState', function (otFacetsState) {
         'use strict';
 
         return {
-
             restrict: 'AE',
-
             scope: {},
-
             templateUrl: 'src/components/facets/facets.html',
-
             link: function (scope) {
-                // scope.dataDistribution =
-                scope.filters = otFacetsFilters.getFilters();
-                scope.selectedFilters = otFacetsFilters.getSelectedFilters();
-                scope.deselectAll = otFacetsFilters.deselectAll;
-
-                // scope.respStatus = 1; //otFacetsFilters.status(); // TODO: handle response status
-                // scope.updateFilter = function(id){
-                //    otFacetsFilters.getFilter(id).toggle();
-                // }
+                scope.facets = otFacetsState.getPageFacets();
             }
-
         };
     }]);
