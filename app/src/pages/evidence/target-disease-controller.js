1294702cb832d1f6aae86363e9202c5e2a6fae03
Luca Fumis <lucaf@ebi.ac.uk>
2017-09-08 17:44:29 +0100

    Moved mouse table to directive in preparation for plugin

--- app/src/pages/evidence/target-disease-controller.js
+++ app/src/pages/evidence/target-disease-controller.js
@@ -436,6 +436,12 @@ angular.module('otControllers')
 
 
         var getRareDiseaseData = function () {
+            $scope.target = $scope.search.target;
+            $scope.disease = $scope.search.disease;
+        };
+
+
+        var getRareDiseaseData = function () {
             $scope.search.tables.genetic_associations.rare_diseases.is_loading = true;
             var opts = {
                 target: $scope.search.target,
@@ -664,156 +670,9 @@ angular.module('otControllers')
         //  M O U S E   D A T A
         // =================================================
 
-        /*
-        Probability:
-        evidence.association_scrore.probability.value
-
-        Mouse phenotypes:
-        show the values for each key (e.g. circling, imapired balance, deafness, etc)
-        evidence.properties.evidence_chain[1].biological object.properties.experiment_specific
-
-        Human phenotypes:
-        same as for moouse phenotypes
-        biological object.properties.experiment specific
-        */
-
         var getMouseData = function () {
-            $scope.search.tables.animal_models.is_loading = true;
-            var opts = {
-                target: $scope.search.target,
-                disease: $scope.search.disease,
-                size: 1000,
-                datasource: $scope.search.tables.animal_models.source, // otConfig.evidence_sources.animal_model,
-                fields: [
-                    'disease',
-                    'evidence',
-                    'scores',
-                    'access_level'
-                ]
-            };
-            _.extend(opts, searchObj);
-
-            var queryObject = {
-                method: 'GET',
-                params: opts
-            };
-            return otApi.getFilterBy(queryObject)
-                .then(
-                    function (resp) {
-                        if (resp.body.data) {
-                            $scope.search.tables.animal_models.data = resp.body.data;
-                            initTableMouse();
-                        } else {
-                            $log.warn('Empty response : animal models data');
-                        }
-                    },
-                    otApi.defaultErrorHandler
-                )
-                .finally(function () {
-                    $scope.search.tables.animal_models.is_loading = false;
-                });
-        };
-
-
-        /*
-         *
-         */
-        var formatMouseDataToArray = function (data) {
-            var newdata = [];
-            data.forEach(function (item) {
-                // create rows:
-                var row = [];
-
-                try {
-                    // data origin: public / private
-                    row.push((item.access_level === otConsts.ACCESS_LEVEL_PUBLIC) ? accessLevelPublic : accessLevelPrivate);
-
-                    // disease
-                    row.push(item.disease.efo_info.label);    // or item.disease.efo_info.label ???
-
-                    // human
-                    row.push('<ul>' + item.evidence.disease_model_association.human_phenotypes.map(function (hp) { return '<li>' + hp.label + '</li>'; }).join('') + '</ul>');
-
-                    // mouse
-                    row.push('<ul>' + item.evidence.disease_model_association.model_phenotypes.map(function (hp) { return '<li>' + hp.label + '</li>'; }).join('') + '</ul>');
-
-                    // mouse model
-                    var mousemodel = processMouseModelLinks(item.evidence.biological_model.allelic_composition, item.evidence.biological_model.allele_ids)
-                                     + '<br/ >'
-                                     + '<span class=\'small text-lowlight\'>' + item.evidence.biological_model.genetic_background + '</span>';
-                    row.push(mousemodel);
-
-
-                    // evidence source
-                    row.push(otDictionary.PHENODIGM);
-
-                    // score -- hidden column now
-                    row.push((item.scores.association_score).toFixed(2));
-
-
-                    newdata.push(row); // push, so we don't end up with empty rows
-                } catch (e) {
-                    $scope.search.tables.animal_models.has_errors = true;
-                    $log.error('Error parsing mouse data:');
-                    $log.error(e);
-                }
-            });
-
-            return newdata;
-        };
-
-
-        var initTableMouse = function () {
-            $('#mouse-table').DataTable(otUtils.setTableToolsParams({
-                'data': formatMouseDataToArray($scope.search.tables.animal_models.data),
-                'autoWidth': false,
-                'paging': true,
-                'ordering': true,
-                'order': [[6, 'des']],
-                'columnDefs': [
-                    {
-                        'targets': [0],    // the access-level (public/private icon)
-                        'visible': otConfig.show_access_level,
-                        'width': '3%'
-                    },
-                    {
-                        'targets': [6],    // score
-                        'visible': false
-                    },
-                    {
-                        'targets': [2, 3, 4],
-                        'width': '20%'
-                    },
-                    {
-                        'targets': [5],
-                        'width': '10%'
-                    }
-                ]
-            }, $scope.search.info.title + '-mouse_models'));
-        };
-
-
-        /*
-         * Takes a string like "Casr<Nuf>/Casr<+>" and returns "Casr<sup>Nuf</sup>/Casr<sup>+</sup>"
-         */
-        var processMouseModelData = function (mmd) {
-            return mmd.replace(/<(.*?)>/g, function (match) { return '<sup>' + match.substr(1, match.length - 2) + '</sup>'; });
-        };
-
-
-        /*
-         * Takes a string like "Casr<Nuf>/Casr<+>" and a string of ids like "MGI:3054788|MGI:3054788"
-         * returns the original string with <a href> tags around each part "Casr<Nuf>" and "Casr<+>"
-         */
-        var processMouseModelLinks = function (mmd, id) {
-            var mmds = mmd.split('/');
-            var ids = id.split('|');
-            for (var i = 0; i < mmds.length; i++) {
-                if (ids[i]) {
-                    mmds[i] = '<a href=\'http://informatics.jax.org/accession/' + ids[i] + '\' target=\'_blank\'>' + processMouseModelData(mmds[i]) + '</a>';
-                }
-            }
-            return mmds.join('/');
+            $scope.target = $scope.search.target;
+            $scope.disease = $scope.search.disease;
         };
 
 
