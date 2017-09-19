44c2bf5a97adceb4c6463e05bf29da3fa36ad738
Miguel Pignatelli <emepyc@gmail.com>
2017-09-19 11:01:38 +0100

    fixed gtex plot + new minor version bumped up

--- app/plugins/rna-baseline-expression-directive.js
+++ app/plugins/rna-baseline-expression-directive.js
@@ -31,7 +31,7 @@ angular.module('plugins')
                     // var url = '/proxy/www.gtexportal.org/api/v6p/expression/' + target + '?boxplot=true';
                     $http.get(url)
                         .then(function (resp) {
-                            var arr = obj2array(resp.data.generpkm);
+                            var arr = obj2array(resp.data.genetpm);
                             var svg = d3.select("#gtexWidget")
                                 .append("svg")
                                 .attr("width", (w-150))
