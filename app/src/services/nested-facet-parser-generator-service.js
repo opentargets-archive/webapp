35f967742591cccdb97792d5c8fca802d53842a4
peatroot <garethpeat@gmail.com>
2017-09-29 14:00:28 +0100

    Preprocess facet api data within otNestedFacetParserGeneraor (to filter out self-child buckets)

--- app/src/services/nested-facet-parser-generator-service.js
+++ app/src/services/nested-facet-parser-generator-service.js
@@ -6,12 +6,12 @@ angular.module('otFacets')
                 var nestedFilters;
 
                 /**
-       * getExistingFilter
-       * Check if a filter with a specific key has already been created
-       * and, if so, retrieve it.
-       * @param {string} key - The key of the filter to search for
-       * @param {array} flatFilters - The (flat) array of filters to look in
-       */
+                 * getExistingFilter
+                 * Check if a filter with a specific key has already been created
+                 * and, if so, retrieve it.
+                 * @param {string} key - The key of the filter to search for
+                 * @param {array} flatFilters - The (flat) array of filters to look in
+                 */
                 var getExistingFilter = function (key, flatFilters) {
                     var match = flatFilters.filter(function (filter) {
                         return filter.key === key;
@@ -20,18 +20,18 @@ angular.module('otFacets')
                 };
 
                 /**
-       * constructFilters
-       * Parse the API data and recursively build a nested filter structure.
-       * @param {object} data - The API data
-       * @param {array} flatFilters - The (flat) array of filters (mutated by function)
-       * @param {object} parent - The parent (or null) of the current level of filters.
-       * 
-       * Nested filters such as target-class can have common children.
-       * For example, "Kinase" appears under "Enzyme" and under
-       * "Other cytosolic protein". For this reason, when constructing
-       * the filters from the apiData, the strategy is to keep a flat
-       * list of filters, which the nested filters reference.
-       */
+                 * constructFilters
+                 * Parse the API data and recursively build a nested filter structure.
+                 * @param {object} data - The API data
+                 * @param {array} flatFilters - The (flat) array of filters (mutated by function)
+                 * @param {object} parent - The parent (or null) of the current level of filters.
+                 * 
+                 * Nested filters such as target-class can have common children.
+                 * For example, "Kinase" appears under "Enzyme" and under
+                 * "Other cytosolic protein". For this reason, when constructing
+                 * the filters from the apiData, the strategy is to keep a flat
+                 * list of filters, which the nested filters reference.
+                 */
                 var constructFilters = function (data, flatFilters, parent) {
                     // iterate the buckets, checking for existance in the flat list
                     // and constructing a new Filter if there is none
@@ -67,9 +67,32 @@ angular.module('otFacets')
                     });
                 };
 
+                var cleanSelfNesting = function (data) {
+                    // Look for <parent>:<child> relationships by key
+                    // If A:A form is found, A should be a parent only
+                    // so remove as a child from itself and from other
+                    // parents too
+
+                    // find the selfies
+                    var selfies = data.buckets.filter(function (parent) {
+                        return parent[accessor].buckets.some(function (child) {
+                            return (parent.key === child.key);
+                        });
+                    }).map(function (bucket) {
+                        return bucket.key;
+                    });
+
+                    // remove as all child occurrences
+                    data.buckets.map(function (parent) {
+                        parent[accessor].buckets = parent[accessor].buckets.filter(function (child) {
+                            return !(selfies.indexOf(child.key) > -1);
+                        });
+                    });
+                };
+
                 /**
-       * Toggle method for all filters
-       */
+                 * Toggle method for all filters
+                 */
                 var setAllChecked = function (value) {
                     // update state
                     nestedFilters.forEach(function (filter) {
@@ -80,10 +103,10 @@ angular.module('otFacets')
                 };
 
                 /**
-       * Serialize this facet for the url state.
-       * @param {object} urlObj - The URL object. This object can be mutated and must
-       *                          then be returned.
-       */
+                 * Serialize this facet for the url state.
+                 * @param {object} urlObj - The URL object. This object can be mutated and must
+                 *                          then be returned.
+                 */
                 var serialize = function (urlObj) {
                     urlObj[facetName] = flatFilters.filter(function (filter) {
                         return filter.checked;
@@ -94,9 +117,11 @@ angular.module('otFacets')
                 };
 
                 /**
-       * Initialize
-       */
+                 * Initialize
+                 */
                 var init = function () {
+                    cleanSelfNesting(apiData[facetName]);
+
                     // setup the nested filters from the api data (structure)
                     nestedFilters = constructFilters(apiData[facetName], flatFilters, null);
 
@@ -105,12 +130,12 @@ angular.module('otFacets')
                 };
 
                 /**
-       * Check if a boolean filter is checked according to the URL object.
-       * Note that the key is an number, but the urlObj may be a string (
-       * ie. 6 vs '6')
-       * @param {object} urlObj - Object representation of the URL
-       * @param {string} key - Key of the filter to check
-       */
+                 * Check if a boolean filter is checked according to the URL object.
+                 * Note that the key is an number, but the urlObj may be a string (
+                 * ie. 6 vs '6')
+                 * @param {object} urlObj - Object representation of the URL
+                 * @param {string} key - Key of the filter to check
+                 */
                 var filterIsChecked = function (urlObj, key) {
                     return (urlObj &&
               urlObj[facetName] &&
@@ -119,9 +144,9 @@ angular.module('otFacets')
                 };
 
                 /**
-       * Deserialize the url state and update the facet state.
-       * @param {object} urlObj - The URL object
-       */
+                 * Deserialize the url state and update the facet state.
+                 * @param {object} urlObj - The URL object
+                 */
                 var deserialize = function (urlObj) {
                     nestedFilters.forEach(function (filter) {
                         filter.setChecked(filterIsChecked(urlObj, filter.key));
