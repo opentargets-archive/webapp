5b063f37c492f46f378ffc29060473699e9a6bbf
Priyanka Wankar <priyanka.wankar@biogen.com>
2017-10-05 15:28:03 -0400

    Remove nferx ref from original text mining plugin

--- app/plugins/text-mining/text-mining-directive.js
+++ app/plugins/text-mining/text-mining-directive.js
@@ -15,7 +15,6 @@ angular.module('otPlugins')
             },
             link: function (scope) {
                 scope.ext = scope.ext || {};    // object to communicate
-                scope.ext.nferx = {};
                 scope.sources = otConfig.evidence_sources.literature.map(function (s) { return {label: otDictionary[otConsts.invert(s)], url: otConsts.dbs_info_url[otConsts.invert(s)]}; });
             }
         };
