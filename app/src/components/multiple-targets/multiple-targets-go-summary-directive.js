55f65cc71d4359a340fde170272c0514c8a16d6c
Miguel Pignatelli <emepyc@gmail.com>
2017-11-02 13:30:14 +0000

    keeping only 1 version of GO term enrichment in batch search results

--- app/src/components/multiple-targets/multiple-targets-go-summary-directive.js
+++ app/src/components/multiple-targets/multiple-targets-go-summary-directive.js
@@ -1,64 +1,45 @@
 angular.module('otDirectives')
-    .directive('multipleTargetsGoSummary', ['$log', 'otApi', '$http', 'otUtils', function ($log, otApi, $http, otUtils) {
+    .directive('multipleTargetsGoSummary', ['$log', '$http', 'otUtils', function ($log, $http, otUtils) {
 
-        function formatGoDataToArray(targetsByGo, goArr) {
-            var data = [];
-
-            for (var i=0; i<goArr.length; i++) {
-                var row = [];
-                var go = goArr[i];
-
-                // GO term id
-                row.push(go.key);
-
-                // GO term description
-                row.push(targetsByGo[go.key].term);
-
-                // GO category
-                var category;
-                var symbol = targetsByGo[go.key].category;
-                if (symbol === 'P') {
-                    category = 'Biological Process';
-                } else if (symbol === 'C') {
-                    category = 'Cellular component';
-                } else if (symbol === 'F') {
-                    category = 'Molecular Function';
-                }
-                row.push(category);
-
-                // Go term pvalue
-                row.push(1);
-
-                // GO term number of targets
-                row.push(go.doc_count);
-
-                // Go targets
-                row.push(targetsByGo[go.key].targets.join(', '));
-
-                data.push(row);
-            }
-            return data;
-        }
-        
-        function getTargetsByGo(data) {
-            var tbg = {};
+        function parseGOdata(data) {
+            var tableData = [];
             for (var i=0; i<data.length; i++) {
-                var d = data[i];
-                for (var j=0; j<d.go.length; j++) {
-                    var g = d.go[j];
-                    if (!tbg[g.id]) {
-                        var termArr = g.value.term.split(':');
-                        tbg[g.id] = {
-                            targets: [],
-                            id: g.id,
-                            category: termArr[0],
-                            term: termArr[1]
-                        };
+                var dataRow = data[i];
+                var dataFields = dataRow.split('\t');
+                if (dataFields[8] && (dataFields[8].substring(0, 3) === 'GO:')) {
+                    var tableRow = [];
+                    // GO term id
+                    tableRow.push(dataFields[8]);
+
+                    // GO term description
+                    tableRow.push(dataFields[11]);
+
+                    // GO category
+                    var category;
+                    var symbol = dataFields[9];
+                    if (symbol === 'BP') {
+                        category = 'Biological Process';
+                    } else if (symbol === 'CC') {
+                        category = 'Cellular component';
+                    } else if (symbol === 'MF') {
+                        category = 'Molecular Function';
                     }
-                    tbg[g.id].targets.push(d.approved_symbol);
+                    tableRow.push(category);
+
+                    // GO term pvalue
+                    tableRow.push(dataFields[2]);
+
+                    var targetsInTerm = dataFields[13].split(',');
+                    // GO term number of targets
+                    tableRow.push(targetsInTerm.length);
+
+                    // GO targets
+                    tableRow.push(targetsInTerm.join(', '));
+
+                    tableData.push(tableRow);
                 }
             }
-            return tbg;
+            return tableData;
         }
 
         return {
@@ -75,47 +56,37 @@ angular.module('otDirectives')
                     }
                     scope.showSpinner = true;
 
-                    var targetIds = scope.target.map(function (t) {
-                        return t.ensembl_gene_id;
-                    });
+                    // var targetIds = scope.target.map(function (t) {
+                    //     return t.ensembl_gene_id;
+                    // });
 
-                    // Get the GO facet for the multiple targets
-                    var opts = {
-                        'facets': 'true',
-                        'fields': ['approved_symbol', 'ensembl_gene_id', 'go'],
-                        'id': targetIds
-                    };
-                    var queryObject = {
-                        method: 'POST',
-                        params: opts
-                    };
+                    // Call gProfiler to get enriched GO terms
+                    // curl -X POST -d 'output=mini&organism=hsapiens&significant=1&sort_by_structure=1&ordered_query=0&as_ranges=0&no_iea=0&underrep=0&hierfiltering=none&user_thr=1&min_set_size=0&max_set_size=0&threshold_algo=analytical&domain_size_type=annotated&query=braf+pten+brca1+brca2' http://biit.cs.ut.ee/gprofiler/
+                    var opts = 'output=mini&organism=hsapiens&significant=1&sort_by_structure=1&ordered_query=0&as_ranges=0&no_iea=0&underrep=0&hierfiltering=none&user_thr=1&min_set_size=0&max_set_size=0&threshold_algo=analytical&domain_size_type=annotated&query=' + (scope.target.map(function(d){return d.approved_symbol}).join('+'));
 
-                    otApi.getTarget(queryObject)
+                    var httpConfig = {
+                        headers: {
+                            'Content-Type': 'application/x-www-form-urlencoded; charset=UTF-8'
+                        }
+                    };
+                    $http.post('https://biit.cs.ut.ee/gprofiler/', opts, httpConfig)
                         .then (function (resp) {
                             scope.showSpinner = false;
-
-                            scope.go = resp.body.facets.significant_go_terms.buckets;
-
-                            // For each aggregated GO term, the api response only gives you
-                            // the number of targets in your query associated with that term
-                            // we parse the data response to get the actual names
-                            var targetsByGo = getTargetsByGo(resp.body.data);
-
-                            $('#target-list-go-terms').DataTable(otUtils.setTableToolsParams({
-                                'data': formatGoDataToArray(targetsByGo, scope.go),
+                            var data = resp.data;
+                            $('#target-list-go2-terms').DataTable(otUtils.setTableToolsParams({
+                                'data': parseGOdata(data.split('\n')),
                                 'ordering': true,
-                                'order': [[3, 'desc']],
+                                'order': [[3, 'asc']],
                                 'autoWidth': false,
                                 'paging': true,
                                 'columnDefs': [
                                     {
-                                        targets: [0, 3],
+                                        targets: [0],
                                         visible: false
                                     }
                                 ]
 
                             }, scope.target.length + '-targets-enriched-go-terms'));
-
                         });
                 })
             }
