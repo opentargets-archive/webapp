391dff54f4763f032deee3048d56fc2515968155
Luca Fumis <lucaf@ebi.ac.uk>
2017-11-29 12:36:22 +0000

    Only show rows with phenotype data + fixed no-data js error

--- app/plugins/mouse-phenotypes/mouse-phenotypes-directive.js
+++ app/plugins/mouse-phenotypes/mouse-phenotypes-directive.js
@@ -2,68 +2,71 @@ angular.module('otPlugins')
     .directive('otMousePhenotypes', ['otColumnFilter', 'otUtils', '$timeout', function (otColumnFilter, otUtils, $timeout) {
         'use strict';
 
-        function formatPhenotypesToArray(data) {
+        function formatPhenotypesToArray (data) {
             var newData = [];
-            data.forEach(function (d) {
-                d.phenotypes.forEach(function (p) {
+            if (data) {
+                data.forEach(function (d) {
+                    d.phenotypes.forEach(function (p) {
+                        if (p.genotype_phenotype && p.genotype_phenotype.length > 0) {
+                            p.genotype_phenotype.forEach(function (g) {
+                                var row = [];
+                                // Mouse gene
+                                row.push('<a target="_blank" href="http://www.informatics.jax.org/marker/' + d.mouse_gene_id + '">' + d.mouse_gene_symbol + '</a>');
 
-                    if (p.genotype_phenotype && p.genotype_phenotype.length) {
-                        p.genotype_phenotype.forEach(function (g) {
+                                // Phenotype category
+                                row.push(p.category_mp_label);
+
+                                // Phenotype label
+                                row.push(g.mp_label);
+
+                                // Allelic composition
+                                row.push(
+                                    g.subject_allelic_composition.split(',')
+                                        .map(function (allele) {
+                                            return otUtils.allelicComposition2Html(allele);
+                                        })
+                                        .join('<br />') +
+                                    'bob'
+                                );
+
+                                // Genetic background
+                                row.push(g.subject_background);
+
+                                // References
+                                if (g.pmid) {
+                                    row.push(otUtils.getPublicationsString(g.pmid.split(',')));
+                                } else {
+                                    row.push('N/A');
+                                }
+
+                                // hidden columns for filtering
+                                row.push(d.mouse_gene_symbol); // variant
+
+                                newData.push(row);
+                            });
+                        } else {
                             var row = [];
-                            // Mouse gene
                             row.push('<a target="_blank" href="http://www.informatics.jax.org/marker/' + d.mouse_gene_id + '">' + d.mouse_gene_symbol + '</a>');
 
                             // Phenotype category
                             row.push(p.category_mp_label);
 
-                            // Phenotype label
-                            row.push(g.mp_label);
-
-                            // Allelic composition
-                            row.push(
-                                g.subject_allelic_composition.split(',')
-                                    .map(function (allele) {
-                                        return otUtils.allelicComposition2Html(allele);
-                                    })
-                                    .join('<br />')
-                            );
-
-                            // Genetic background
-                            row.push(g.subject_background);
-
-                            // References
-                            if (g.pmid) {
-                                row.push(otUtils.getPublicationsString(g.pmid.split(',')));
-                            } else {
-                                row.push('N/A');
-                            }
+                            // fill with N/A
+                            row.push('N/A');
+                            row.push('N/A');
+                            row.push('N/A');
+                            row.push('N/A');
 
                             // hidden columns for filtering
                             row.push(d.mouse_gene_symbol); // variant
+                            
+                            // LUCA: just show rows with phenotype information to avoid N/A rows...
+                            // newData.push(row);
+                        }
+                    });
 
-                            newData.push(row);
-                        })
-                    } else {
-                        var row = [];
-                        row.push('<a target="_blank" href="http://www.informatics.jax.org/marker/' + d.mouse_gene_id + '">' + d.mouse_gene_symbol + '</a>');
-
-                        // Phenotype category
-                        row.push(p.category_mp_label);
-
-                        // fill with N/A
-                        row.push('N/A');
-                        row.push('N/A');
-                        row.push('N/A');
-                        row.push('N/A');
-
-                        // hidden columns for filtering
-                        row.push(d.mouse_gene_symbol); // variant
-                        newData.push(row);
-                    }
                 });
-
-            });
-
+            };
             return newData;
         }
 
