e1414ce7852e1fa2ec733c613208338d8bb433e6
Miguel Pignatelli <emepyc@gmail.com>
2017-12-01 18:38:49 +0000

    catch errors on unknown drugs

--- app/src/components/drug-summary/drug-summary-directive.js
+++ app/src/components/drug-summary/drug-summary-directive.js
@@ -52,118 +52,117 @@ angular.module('otDirectives')
                                 });
                             }
                             return scope.drug;
+                        }, function (err) {
+                            scope.noDrug = true;
                         })
-                    .then(function (drugId) {
+                        .then(function (drugId) {
 
-                        // Get the mechanism of action...
-                        $http.get('https://www.ebi.ac.uk/chembl/api/data/molecule_form/' + drugId)
-                            .then(function (resp) {
-                                var molForms = {};
-                                for (var i = 0; i < resp.data.molecule_forms.length; i++) {
-                                    var form = resp.data.molecule_forms[i];
-                                    molForms[form.molecule_chembl_id] = true;
-                                    molForms[form.parent_chembl_id] = true;
-                                }
-                                var promises = [];
-                                Object.keys(molForms).forEach(function (mol) {
-                                    promises.push($http.get('https://www.ebi.ac.uk/chembl/api/data/mechanism?molecule_chembl_id=' + mol));
-                                });
-                                $q.all(promises)
-                                    .then(function (resps) {
-                                        var allMecs = [];
-                                        scope.mechanisms = allMecs;
-                                        for (var i = 0; i < resps.length; i++) {
-                                            var mecs = resps[i].data.mechanisms;
-                                            for (var j = 0; j < mecs.length; j++) {
-                                                var mec = mecs[j].mechanism_of_action;
-                                                var target = mecs[j].target_chembl_id;
-                                                var refs = mecs[j].mechanism_refs;
-                                                (function (mec, refs) { // make sure mec and refs are passed to the closure and are not mutated while making the calls
-                                                    $http.get('https://www.ebi.ac.uk/chembl/api/data/target?target_chembl_id=' + target)
-                                                        .then(function (resp) {
-                                                            var targetNames = [];
-                                                            for (var i = 0; i < resp.data.targets.length; i++) {
-                                                                var target = resp.data.targets[i];
-                                                                var uniqSyns = {};
-                                                                for (var j = 0; j < target.target_components.length; j++) {
-                                                                    var component = target.target_components[j];
-                                                                    for (var k = 0; k < component.target_component_synonyms.length; k++) {
-                                                                        var synonym = component.target_component_synonyms[k];
-                                                                        if (synonym.syn_type === 'GENE_SYMBOL') {
-                                                                            uniqSyns[synonym.component_synonym.trim()] = {};
+                            // Get the mechanism of action...
+                            $http.get('https://www.ebi.ac.uk/chembl/api/data/molecule_form/' + drugId)
+                                .then(function (resp) {
+                                    var molForms = {};
+                                    for (var i = 0; i < resp.data.molecule_forms.length; i++) {
+                                        var form = resp.data.molecule_forms[i];
+                                        molForms[form.molecule_chembl_id] = true;
+                                        molForms[form.parent_chembl_id] = true;
+                                    }
+                                    var promises = [];
+                                    Object.keys(molForms).forEach(function (mol) {
+                                        promises.push($http.get('https://www.ebi.ac.uk/chembl/api/data/mechanism?molecule_chembl_id=' + mol));
+                                    });
+                                    $q.all(promises)
+                                        .then(function (resps) {
+                                            var allMecs = [];
+                                            scope.mechanisms = allMecs;
+                                            for (var i = 0; i < resps.length; i++) {
+                                                var mecs = resps[i].data.mechanisms;
+                                                for (var j = 0; j < mecs.length; j++) {
+                                                    var mec = mecs[j].mechanism_of_action;
+                                                    var target = mecs[j].target_chembl_id;
+                                                    var refs = mecs[j].mechanism_refs;
+                                                    (function (mec, refs) { // make sure mec and refs are passed to the closure and are not mutated while making the calls
+                                                        $http.get('https://www.ebi.ac.uk/chembl/api/data/target?target_chembl_id=' + target)
+                                                            .then(function (resp) {
+                                                                var targetNames = [];
+                                                                for (var i = 0; i < resp.data.targets.length; i++) {
+                                                                    var target = resp.data.targets[i];
+                                                                    var uniqSyns = {};
+                                                                    for (var j = 0; j < target.target_components.length; j++) {
+                                                                        var component = target.target_components[j];
+                                                                        for (var k = 0; k < component.target_component_synonyms.length; k++) {
+                                                                            var synonym = component.target_component_synonyms[k];
+                                                                            if (synonym.syn_type === 'GENE_SYMBOL') {
+                                                                                uniqSyns[synonym.component_synonym.trim()] = {};
+                                                                            }
                                                                         }
                                                                     }
-                                                                }
-                                                                // Try to find the synonyms in ot
-                                                                var opts = {
-                                                                    q: Object.keys(uniqSyns),
-                                                                    filter: 'target'
-                                                                };
+                                                                    // Try to find the synonyms in ot
+                                                                    var opts = {
+                                                                        q: Object.keys(uniqSyns),
+                                                                        filter: 'target'
+                                                                    };
 
-                                                                var queryObject = {
-                                                                    method: 'POST',
-                                                                    params: opts
-                                                                };
-                                                                (function (uniqSyns, target) { // make sure uniqSyns and target and passed to the closure and don't mutate
-                                                                    otApi.getBestHitSearch(queryObject)
-                                                                        .then (function (resp) {
-                                                                            var uniqSynsArr = Object.keys(uniqSyns).map(function(s) {
-                                                                                return {
-                                                                                    synonym: s
-                                                                                };
-                                                                            });
-                                                                            for (var i = 0; i < resp.body.data.length; i++) {
-                                                                                var q = resp.body.data[i];
-                                                                                if (q.id) {
-                                                                                    uniqSynsArr[i].ensId = q.id
+                                                                    var queryObject = {
+                                                                        method: 'POST',
+                                                                        params: opts
+                                                                    };
+                                                                    (function (uniqSyns, target) { // make sure uniqSyns and target and passed to the closure and don't mutate
+                                                                        otApi.getBestHitSearch(queryObject)
+                                                                            .then(function (resp) {
+                                                                                var uniqSynsArr = Object.keys(uniqSyns).map(function (s) {
+                                                                                    return {
+                                                                                        synonym: s
+                                                                                    };
+                                                                                });
+                                                                                for (var i = 0; i < resp.body.data.length; i++) {
+                                                                                    var q = resp.body.data[i];
+                                                                                    if (q.id) {
+                                                                                        uniqSynsArr[i].ensId = q.id
+                                                                                    }
                                                                                 }
-                                                                            }
-                                                                            return uniqSynsArr;
-                                                                        })
-                                                                        .then (function (uniqSynsArr) {
-                                                                            targetNames.push(target.pref_name);
-                                                                            allMecs.push({
-                                                                                mechanism: mec,
-                                                                                targets: targetNames.join(', '),
-                                                                                synonyms: uniqSynsArr,
-                                                                                refs: refs
+                                                                                return uniqSynsArr;
+                                                                            })
+                                                                            .then(function (uniqSynsArr) {
+                                                                                targetNames.push(target.pref_name);
+                                                                                allMecs.push({
+                                                                                    mechanism: mec,
+                                                                                    targets: targetNames.join(', '),
+                                                                                    synonyms: uniqSynsArr,
+                                                                                    refs: refs
+                                                                                });
                                                                             });
-                                                                        });
-                                                                })(uniqSyns, target);
-                                                            }
-                                                        });
-                                                })(mec, refs);
+                                                                    })(uniqSyns, target);
+                                                                }
+                                                            });
+                                                    })(mec, refs);
+                                                }
                                             }
-                                        }
-                                    });
-                            }, function (err) {
-                                scope.noDrug = true;
-                            });
+                                        });
+                                });
 
-                        // Get the information for the drug...
-                        return $http.get('https://www.ebi.ac.uk/chembl/api/data/molecule/' + drugId)
-                            .then(function (resp) {
-                                scope.displayName = resp.data.pref_name || resp.data.molecule_chembl_id;
-                                // scope.mechanism = resp.data.usan_stem_definition || 'NA';
-                                scope.mol_type = resp.data.molecule_type || 'NA';
-                                scope.first_approval = resp.data.first_approval || 'NA';
-                                scope.max_phase = resp.data.max_phase || 'NA';
-                                if (resp.data.molecule_properties && resp.data.molecule_properties.full_molformula) {
-                                    scope.formula = resp.data.molecule_properties.full_molformula;
-                                } else {
-                                    scope.formula = 'NA';
-                                }
+                            // Get the information for the drug...
+                            return $http.get('https://www.ebi.ac.uk/chembl/api/data/molecule/' + drugId)
+                                .then(function (resp) {
+                                    scope.displayName = resp.data.pref_name || resp.data.molecule_chembl_id;
+                                    // scope.mechanism = resp.data.usan_stem_definition || 'NA';
+                                    scope.mol_type = resp.data.molecule_type || 'NA';
+                                    scope.first_approval = resp.data.first_approval || 'NA';
+                                    scope.max_phase = resp.data.max_phase || 'NA';
+                                    if (resp.data.molecule_properties && resp.data.molecule_properties.full_molformula) {
+                                        scope.formula = resp.data.molecule_properties.full_molformula;
+                                    } else {
+                                        scope.formula = 'NA';
+                                    }
 
-                                if (scope.mol_type !== 'Antibody') {
-                                    pngToDataUrl('https://www.ebi.ac.uk/chembl/api/data/image/' + drugId, function (base64Img) {
-                                        var img = document.getElementById('drugDiagramContainer');
-                                        img.setAttribute('src', base64Img);
-                                    });
-                                }
-                                return scope.displayName;
-                            });
-
-                    })
+                                    if (scope.mol_type !== 'Antibody') {
+                                        pngToDataUrl('https://www.ebi.ac.uk/chembl/api/data/image/' + drugId, function (base64Img) {
+                                            var img = document.getElementById('drugDiagramContainer');
+                                            img.setAttribute('src', base64Img);
+                                        });
+                                    }
+                                    return scope.displayName;
+                                })
+                        })
                         .then(function (drugName) {
                             if (!drugName) {
                                 return;
@@ -177,7 +176,7 @@ angular.module('otDirectives')
                                     filter: 'target'
                                 }
                             })
-                                .then (function (targetsResp) {
+                                .then(function (targetsResp) {
                                     scope.targets = targetsResp.body.data.map(function (t) {
                                         return {
                                             id: t.id,
@@ -187,7 +186,9 @@ angular.module('otDirectives')
                                         return (a.name < b.name) ? -1 : (a.name > b.name) ? 1 : 0;
                                     });
                                     if (scope.targets.length > 1) {
-                                        scope.batchSearchTargets = otUtils.compressTargetIds(scope.targets.map(function (d) { return d.id })).join(',');
+                                        scope.batchSearchTargets = otUtils.compressTargetIds(scope.targets.map(function (d) {
+                                            return d.id
+                                        })).join(',');
                                     }
                                 });
 
@@ -200,7 +201,7 @@ angular.module('otDirectives')
                                     filter: 'disease'
                                 }
                             })
-                                .then (function (diseasesResp) {
+                                .then(function (diseasesResp) {
                                     scope.diseases = diseasesResp.body.data.map(function (d) {
                                         return {
                                             id: d.id,
