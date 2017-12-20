adc4b8fd307f6de9c061fd5059093ac09ce18ff3
peatroot <garethpeat@gmail.com>
2017-12-20 08:09:47 +0000

    Inject cttvApi to postgapView

--- app/plugins/postgap/postgap-directive.js
+++ app/plugins/postgap/postgap-directive.js
@@ -1,17 +1,18 @@
 angular.module('otPlugins')
-    .directive('otPostgap', function () {
-       return {
-           restrict: 'E',
-           templateUrl: 'plugins/postgap/postgap.html',
-           scope: {
-               target: '=',
-               width: '='
-           },
-           link: function (scope, elem) {
-               var ps = postgapViewer()
-                   .gene(scope.target.id);
-               console.log(elem[0]);
-               ps(elem[0]);
-           }
-       };
-    });
+    .directive('otPostgap', ['otApi', function (otApi) {
+        return {
+            restrict: 'E',
+            templateUrl: 'plugins/postgap/postgap.html',
+            scope: {
+                target: '=',
+                width: '='
+            },
+            link: function (scope, elem) {
+                var ps = postgapViewer()
+                    .gene(scope.target.id)
+                    .cttvApi(otApi.getSelf());
+                console.log(elem[0]);
+                ps(elem[0]);
+            }
+        };
+    }]);
