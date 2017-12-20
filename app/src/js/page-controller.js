c6391d539f16c179abecf2b9926afa229f8ce0f2
Luca Fumis <lucaf@ebi.ac.uk>
2017-12-20 15:12:09 +0000

    Feedback tab and email popup on HP only; footer form on other pages

--- app/src/js/page-controller.js
+++ app/src/js/page-controller.js
@@ -3,10 +3,11 @@ angular.module('otControllers')
      * A generic page controller;
      * Can be used to pass common variables, constants or services to all pages
      */
-    .controller('PageController', ['$scope', 'otConfig', 'otDictionary', 'otConsts', function ($scope, otConfig, otDictionary, otConsts) {
+    .controller('PageController', ['$scope', 'otConfig', 'otDictionary', 'otConsts', '$location', function ($scope, otConfig, otDictionary, otConsts, $location) {
         'use strict';
         $scope.otConfig = otConfig;
         $scope.otDictionary = otDictionary;
         $scope.otConsts = otConsts;
+        $scope.location = $location;
     }]);
 
