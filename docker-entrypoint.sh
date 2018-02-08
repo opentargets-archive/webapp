2ea4155a1b4a8989e415d8c5989c16f6ea0e6307
Eliseo Papa <elipapa@users.noreply.github.com>
2018-02-08 14:41:41 +0000

    new default server

--- docker-entrypoint.sh
+++ docker-entrypoint.sh
@@ -2,7 +2,7 @@
 
 
 export REST_API_SCHEME=${REST_API_SCHEME:=https}
-export REST_API_SERVER=${REST_API_SERVER:=api.targetvalidation.org:443}
+export REST_API_SERVER=${REST_API_SERVER:=api.opentargets.io:443}
 export ENSEMBL_API_KEY=${ENSEMBL_API_KEY:=YOUR_KEY_HERE}
 
 # substitute the rest API address in the nginx configuration
@@ -11,8 +11,7 @@ envsubst < /etc/nginx/conf.d/app_server.template > /etc/nginx/conf.d/app_server.
 envsubst '$REST_API_SCHEME:$ENSEMBL_API_KEY' < /etc/nginx/conf.d/rest_api_scheme.template > /etc/nginx/conf.d/rest_api_scheme.conf
 
 echo "======================================="
-echo "POINTING TO REST API"
-echo at ${REST_API_SCHEME}://${REST_API_SERVER}
+echo "INFO >>> /api is pointing to : ${REST_API_SCHEME}://${REST_API_SERVER}"
 echo "======================================="
 
 # assume that user wants to run his own process,
