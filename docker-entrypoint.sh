cea9e63525ddb6a76590b80ad2cb0f08a0fb0a3c
Eliseo Papa <elipapa@users.noreply.github.com>
2018-02-09 16:38:25 +0000

    remove redundant lines

--- docker-entrypoint.sh
+++ docker-entrypoint.sh
@@ -8,10 +8,7 @@ export REST_API_PORT=${REST_API_PORT:=443}
 
 # substitute the rest API address in the nginx configuration
 
-# envsubst < /etc/nginx/conf.d/app_server.template > /etc/nginx/conf.d/app_server.conf
 envsubst '$REST_API_SCHEME $REST_API_SERVER $REST_API_PORT' < /etc/nginx/nginx.template > /etc/nginx/nginx.conf
-
-# /etc/nginx/conf.d/rest_api_scheme.template > /etc/nginx/conf.d/rest_api_scheme.conf
 
 echo "======================================="
 echo "INFO >>> /api is pointing to : ${REST_API_SCHEME}://${REST_API_SERVER}:${REST_API_PORT}"
