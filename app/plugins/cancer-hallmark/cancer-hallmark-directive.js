c750170ca109a00faecb927dd6779f1b03392b96
Luca Fumis <lucaf@ebi.ac.uk>
2018-02-21 21:00:02 +0000

    Finalized table view and details for cancer hallmark data

--- app/plugins/cancer-hallmark/cancer-hallmark-directive.js
+++ app/plugins/cancer-hallmark/cancer-hallmark-directive.js
@@ -1,5 +1,5 @@
 angular.module('otPlugins')
-    .directive('otCancerHallmark', [/* 'otColumnFilter', 'otUtils', '$timeout', 'otConfig',*/function (/* otColumnFilter, otUtils, $timeout, otConfig*/) {
+    .directive('otCancerHallmark', ['otUtils', '$timeout', function (otUtils, $timeout) {
         'use strict';
 
         return {
@@ -9,9 +9,7 @@ angular.module('otPlugins')
                 target: '='
             },
             link: function (scope, elem, attrs) {
-                // console.log('bob');
-                // console.log('target: ', scope.target);
-
+                // define the set list of hallmarks as API only returns those with info
                 var hallmarks = [
                     'proliferative signalling',
                     'suppression of growth',
@@ -25,7 +23,9 @@ angular.module('otPlugins')
                     'change of cellular energetics'
                 ];
 
-                // lightweight list to build visualizations
+
+                // lightweight list to build visualizations:
+                // only store label, suppress and promote
                 scope.hallmarks = hallmarks.map(function (m) {
                     var chm = scope.target.hallmarks.cancer_hallmarks.filter(function (ch) {
                         return ch.label === m;
@@ -40,66 +40,68 @@ angular.module('otPlugins')
                 });
 
 
+                // set the selected hallmark when user clicks on table row
+                // selected is just the label
+                // selected list is a list of those matching the label
                 scope.setSelected = function (m) {
-                    console.log('!');
                     scope.selected = m;
                     scope.selectedList = scope.target.hallmarks.cancer_hallmarks.filter(function (ch) {
                         return ch.label === scope.selected;
                     });
-                    console.log('selected: ', scope.selected);
                 };
 
-                scope.getSelected = function () {
-                    return hallmarks.filter(function (ch) {
-                        return ch.label === scope.selected;
+
+                function formatDataToArray () {
+                    var rows = scope.target.hallmarks.cancer_hallmarks.map(function (mark) {
+                        var row = [];
+                        row.push(mark.label);
+                        row.push(mark.description);
+                        var s = '';
+                        s += (mark.promote) ? '<span>Promotes</span>' : '';
+                        s += (mark.promote && mark.suppress) ? '<br />' : '';
+                        s += (mark.suppress) ? '<span>Suppresses</span>' : '';
+                        row.push(s);
+
+                        var l = '<a href="http://europepmc.org/search?query=EXT_ID:' + mark.pmid + '" target="_blank">' + mark.pmid + '</a>';
+                        row.push(l);
+                        return row;
                     });
-                };
 
-                // TODO: targets without phenotypes are giving an empty array instead of an empty object:
-                // https://github.com/opentargets/data_pipeline/issues/221
+                    return rows;
+                }
 
-                // if (!Object.keys(scope.target.mouse_phenotypes).length) {
-                //     scope.noPhenotypes = true;
-                //     return;
-                // }
 
-                // var data = formatPhenotypesToArray(scope.target.mouse_phenotypes);
+                function initTable () {
+                    var table = elem[0].getElementsByTagName('table')[1];
 
-                // scope.data = data;
-                // scope.sources = [{label: 'MGI', url: 'http://www.informatics.jax.org/'}];
+                    $(table).DataTable(otUtils.setTableToolsParams({
+                        'data': formatDataToArray(),
+                        'ordering': true,
+                        // 'order': [[1, 'asc']],
+                        'autoWidth': false,
+                        'paging': true,
+                        'columnDefs': [
+                            {
+                                'targets': [0],
+                                'width': '25%'
+                            },
+                            {
+                                'targets': [1],
+                                'width': '49%'
+                            },
+                            {
+                                'targets': [2, 3],
+                                'width': '13%'
+                            }
+                        ]
+                    }, scope.target.approved_symbol + '-cancer_hallmark'));
+                }
 
-                // var dropdownColumns = [0, 1];
-                // $timeout(function () {
-                //     var table = elem[0].getElementsByTagName('table');
-                //     $(table).dataTable(otUtils.setTableToolsParams({
-                //         'data': data,
-                //         'autoWidth': false,
-                //         'paging': true,
-                //         'order': [[0, 'asc']],
-                //         'columnDefs': [
-                //             {
-                //                 'targets': [0],
-                //                 'mRender': otColumnFilter.mRenderGenerator(5),
-                //                 'mData': otColumnFilter.mDataGenerator(0, 5),
-                //                 'width': '14%'
-                //             },
-                //             // {
-                //             //     'targets': [2, 3, 4],
-                //             //     'width': '22%'
-                //             // },
-                //             // {
-                //             //     'targets': [1],
-                //             //     'width': '15%'
-                //             // }
-                //             {
-                //                 'targets': [1, 2, 3],
-                //                 'width': '24%'
-                //             }
-                //         ],
 
-                //         initComplete: otColumnFilter.initCompleteGenerator(dropdownColumns)
-                //     }));
-                // }, 0);
+                // initialize table in timeout
+                $timeout(function () {
+                    initTable();
+                }, 0);
             }
         };
     }]);
