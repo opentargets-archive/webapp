6a9d1c9872103dd5e8d2a225eeab7600c47419e4
Luca Fumis <lucaf@ebi.ac.uk>
2018-03-29 18:19:08 +0100

    Updating blog api script 3

--- app/src/components/feeds/blog-feed-directive.js
+++ app/src/components/feeds/blog-feed-directive.js
@@ -18,14 +18,14 @@ angular.module('otDirectives')
                 scope.limit = scope.limit || 'all';
                 var url = ghost.url.api('posts', {
                     limit: scope.limit,
-                    include: 'posts, author, tags',
-                    fields: 'title, html, meta_description, published_at, slug, author, tags',
+                    // include: 'posts, author, tags',
+                    // fields: 'title, html, meta_description, published_at, slug, author, tags',
+                    include: 'posts, author',
+                    fields: 'title, html, meta_description, published_at, slug, author',
                     order: 'published_at DESC'
                 });
                 var proxy_url = '/proxy/' + url.substr(2);  // e.g. '/proxy/blog.opentargets.org/ghost/api/v0.1/tags/?limit=all&include=count.posts&order=count.posts%20DESC&client_id=ghost-frontend&client_secret=a9fe83f50655'
                 var href_url = ghost.url.api().split('ghost')[0];
-
-                $http.get(url)
 
                 $http.get(proxy_url)
                     .then(function successCallback (response) {
