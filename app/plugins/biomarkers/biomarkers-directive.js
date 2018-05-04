a48acb6b2bdf549890d38e1ab4b235549042adc7
Luca Fumis <lucaf@ebi.ac.uk>
2018-05-04 18:55:20 +0100

    Updated biomarkers table to latest data version

--- app/plugins/biomarkers/biomarkers-directive.js
+++ app/plugins/biomarkers/biomarkers-directive.js
@@ -12,31 +12,47 @@ angular.module('otPlugins')
                 var dropdownColumns = [0, 1, 2, 3, 4, 5, 6, 7, 8];
 
                 function formatDataToArray () {
-                    var rows = scope.target.cancerbiomarkers.cancer_biomarkers.map(function (mark) {
-                        // Drug
-                        // Disease
-                        // Biomarker
-                        // Individual
-                        // Evidence
-                        // Association
+                    var rows = scope.target.cancerbiomarkers[0].map(function (mark) {
                         var row = [];
 
                         // drug
-                        row.push(mark.drug);
-                        row.push(mark.drugfamily);
                         row.push(mark.drugfullname);
+
+                        // association
+                        row.push(mark.association);
 
                         // disease
                         row.push(mark.disease);
-                        row.push(mark.diseaseID);
 
-                        // biomarker
-                        row.push(mark.biomarker);
-                        row.push(mark.individualbiomarker);
-
+                        // evidence level
                         row.push(mark.evidencelevel);
 
-                        row.push(mark.association);
+                        // references
+                        var ref_string = '';
+
+                        // pubmed
+                        if (mark.references.pubmed && mark.references.pubmed.length > 0) {
+                            ref_string += otUtils.getPublicationsString(
+                                mark.references.pubmed.map(function (pm) {
+                                    return pm.pmid;
+                                })
+                            );
+                        }
+
+                        // other references (abstracts)
+                        if (mark.references.other && mark.references.other.length > 0) {
+                            ref_string += '<p>';
+                            ref_string += mark.references.other.map(function (other) {
+                                return '<a href="' + other.link + '" target="_blank">' + other.name + '</a>';
+                            })
+                            ref_string += '</p>';
+                        }
+
+                        ref_string = ref_string || 'N/A';
+                        row.push(ref_string);
+
+                        // biomarker
+                        row.push(mark.individualbiomarker || mark.biomarker); // emtpy string evaluates to false
 
                         return row;
                     });
@@ -54,25 +70,29 @@ angular.module('otPlugins')
                         'paging': true,
                         'columnDefs': [
                             {
-                                'targets': [0, 1, 2],
-                                'width': '12%'
+                                'targets': [3, 4, 5],
+                                'width': '15%'
                             },
                             {
-                                'targets': [3],
-                                'width': '18%'
+                                'targets': [1],
+                                'width': '13%'
                             },
                             {
-                                'targets': [4],
-                                'width': '6%'
-                            },
-                            {
-                                'targets': [5,6],
-                                'width': '12%'
-                            },
-                            {
-                                'targets': [7,8],
-                                'width': '10%'
+                                'targets': [0, 2],
+                                'width': '21%'
                             }
+                            // {
+                            //     'targets': [3],
+                            //     'width': '18%'
+                            // },
+                            // {
+                            //     'targets': [4],
+                            //     'width': '6%'
+                            // },
+                            // {
+                            //     'targets': [5, 6],
+                            //     'width': '12%'
+                            // }
                         ],
                         initComplete: otColumnFilter.initCompleteGenerator(dropdownColumns)
                     }, scope.target.approved_symbol + '-biomarkers'));
