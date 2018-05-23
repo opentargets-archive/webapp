fd41f056b53e2bd02272d0d7a30484c1ad213845
peatroot <garethpeat@gmail.com>
2018-05-23 12:15:05 +0100

    Fix typo (missing slash)

--- sitemap-generator.js
+++ sitemap-generator.js
@@ -65,11 +65,11 @@ function writeXmlObject (xmlObject, fileStem) {
 }
 
 function createSitemapIndex () {
-    const SITEMAP_STATIC = sitemap('sitemaps/1804/static.xml.gz');
-    const SITEMAP_TARGET_PROFILES = sitemap('sitemaps/1804/target-profiles.xml.gz');
-    const SITEMAP_TARGET_ASSOCS = sitemap('sitemaps/1804/target-associations.xml.gz');
-    const SITEMAP_DISEASE_PROFILES = sitemap('sitemaps/1804/disease-profiles.xml.gz');
-    const SITEMAP_DISEASE_ASSOCS = sitemap('sitemaps/1804/disease-associations.xml.gz');
+    const SITEMAP_STATIC = sitemap('/sitemaps/1804/static.xml.gz');
+    const SITEMAP_TARGET_PROFILES = sitemap('/sitemaps/1804/target-profiles.xml.gz');
+    const SITEMAP_TARGET_ASSOCS = sitemap('/sitemaps/1804/target-associations.xml.gz');
+    const SITEMAP_DISEASE_PROFILES = sitemap('/sitemaps/1804/disease-profiles.xml.gz');
+    const SITEMAP_DISEASE_ASSOCS = sitemap('/sitemaps/1804/disease-associations.xml.gz');
 
     const xmlObject = {
         sitemapindex: [
