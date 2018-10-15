7bea7316908b9b767b79edcb7d49d18eb713fed8
Luca Fumis <lucaf@ebi.ac.uk>
2018-10-15 12:47:21 +0100

    Added tabbed views and faceting functionality to disease association page

--- app/src/pages/disease-associations/disease-associations-controller.js
+++ app/src/pages/disease-associations/disease-associations-controller.js
@@ -14,18 +14,34 @@ angular.module('otControllers')
  * Then when we get the data, we update content and facets
  */
 
-    .controller('DiseaseAssociationsController', ['$scope', '$location', '$q', 'otApi', 'otFacetsState', 'otDictionary', 'otUtils', 'otLocationState', 'otConfig', function ($scope, $location, $q, otApi, otFacetsState, otDictionary, otUtils, otLocationState, otConfig) {
+    .controller('DiseaseAssociationsController', ['$scope', '$location', '$q', 'otApi', 'otFacetsState', 'otDictionary', 'otUtils', 'otLocationState', 'otConfig', 'otConsts', function ($scope, $location, $q, otApi, otFacetsState, otDictionary, otUtils, otLocationState, otConfig, otConsts) {
         'use strict';
 
         otLocationState.init();   // does nothing, but ensures the otLocationState service is instantiated and ready
-
 
         // ---------------------------
         //  Initialiaze
         // ---------------------------
 
 
-        // configure the "search" object
+        // the scope view is essentially the state
+        $scope.view = {
+            t: ['table']    // t = the selected tab
+        };
+
+        // state we want to export to/from the URL
+        var stateId = 'view';
+        var facetsId = otFacetsState.stateId;
+        var facetData;
+        var defaultPrioritizationDatatypes = [
+            otConsts.datatypes.GENETIC_ASSOCIATION.id,
+            otConsts.datatypes.SOMATIC_MUTATION.id,
+            otConsts.datatypes.KNOWN_DRUG.id,
+            otConsts.datatypes.AFFECTED_PATHWAY.id
+        ];
+
+
+        // configure the 'search' object
         // to be exposed via scope
         $scope.search = {
             query: $location.path().split('/')[2],
@@ -47,14 +63,50 @@ angular.module('otControllers')
             // $window.location.reload();
         };
 
-        // set page facets (from config)
-        var facetNames = otConfig.diseaseAssociationsFacets.facets;
-        otFacetsState.setPageFacetNamesFromConfig(facetNames);
+        /*
+         * The view is essentially the state for the page;
+         * filters are autonomous and do their own business
+         */
+        var setView = function (obj) {
+            // should work also for obj==undefined at page load
+            // or if navigating back through browser history
 
+            // define defaults as needed
+            obj = obj || {};
+            obj.t = obj.t || ['table'];
 
-        // state we want to export to/from the URL
-        // var stateId = "view";
-        var facetsId = otFacetsState.stateId;
+            // update the scope; only the tab is needed at the moment
+            $scope.view.t = obj.t;
+            setPageFacets(obj.t[0]);
+        };
+
+        // page facets are actually per 'view' and not strictly per page
+        var setPageFacets = function (v) {
+            var facetsNames = '';
+            v = v || 'table';
+            if (v === 'table') {
+                facetsNames = otConfig.diseaseAssociationsFacets.facets;
+            } else if (v === 'priority') {
+                facetsNames = otConfig.diseasePrioritizationFacets.facets;
+
+                // if no datatypes specified, then we need to set defaults for the 'Show' facet
+                // This is likely NOT the best place nor
+                // most elegant way to do this, but will do for now, so
+                // set the datatypes here
+                var currentfacets = otLocationState.getState()[facetsId];
+
+                if (!currentfacets) {
+                    currentfacets = {};
+                }
+                if (!currentfacets.datatype || currentfacets.datatype.length === 0) {
+                    currentfacets.datatype = defaultPrioritizationDatatypes;
+                    otLocationState.setStateFor(facetsId, currentfacets, true);
+                }
+            }
+
+            otFacetsState.setPageFacetNamesFromConfig(facetsNames);
+        };
+
 
         /*
          * Renders page elements based on state from locationStateService
@@ -64,7 +116,6 @@ angular.module('otControllers')
             // 1. first we check if the state of a particular element has changed;
             // 2. if it hasn't changed, and it's undefined (i.e. new=undefined, old=undefined),
             // then it's a page load with no state specified, so we update that element anyway with default values
-
 
             // Do we have targets?
             var targets;
@@ -79,6 +130,11 @@ angular.module('otControllers')
                 $scope.targets = [];
             }
 
+            // view changed?
+            if (! _.isEqual(new_state[stateId], old_state[stateId]) || !new_state[stateId]) {
+                setView(new_state[stateId]);
+            }
+
             // facets changed?
             var facetsPromise = $q(function (resolve) {
                 resolve('');
@@ -88,21 +144,30 @@ angular.module('otControllers')
                     return $scope.getFacets(new_state[facetsId], $scope.targets);
                 });
             }
+
+            // if view changed, but facets didn't, then try resetting from current data
+            // yes this could be refactored somewhere above here, but it's clear to understand the case here
+            if (
+                (! _.isEqual(new_state[stateId], old_state[stateId]) || !new_state[stateId])
+                && !(!_.isEqual(new_state[facetsId], old_state[facetsId]) || !new_state[facetsId])
+            ) {
+                otFacetsState.updatePageFacetsFromApiData(facetData, otConfig.diseaseAssociationsFacets.count);
+            }
         };
 
 
         /*
          * Get data to populate the table.
          *
-         * @param filters: object of filtering categories, e.g. "datatypes"; each one is either a string or an array of strings
+         * @param filters: object of filtering categories, e.g. 'datatypes'; each one is either a string or an array of strings
          * Example:
          * filters = {
-         *      datatypes: "known_drug",
-         *      pathway_type: [ "REACT_111102", "REACT_116125", "REACT_6900" ]
+         *      datatypes: 'known_drug',
+         *      pathway_type: [ 'REACT_111102', 'REACT_116125', 'REACT_6900' ]
          * }
          * getFacets(filters);
          */
-        $scope.getFacets = function (filters, targetArray) {
+        var getFacets = function (filters, targetArray) {
             // set the filters
             $scope.filters = filters;
 
@@ -130,7 +195,8 @@ angular.module('otControllers')
 
                     if (resp.body.total) {
                         // TODO Change this to POST request
-                        otFacetsState.updatePageFacetsFromApiData(resp.body.facets, otConfig.diseaseAssociationsFacets.count);
+                        facetData = resp.body.facets;
+                        otFacetsState.updatePageFacetsFromApiData(facetData, otConfig.diseaseAssociationsFacets.count);
 
                         // The label of the diseases in the header
                         $scope.search.label = resp.body.data[0].disease.efo_info.label;
@@ -153,6 +219,25 @@ angular.module('otControllers')
                     }
                 }, otApi.defaultErrorHandler);
         };
+        $scope.getFacets = getFacets;
+
+        /*
+         * Update function passes the current view (state) to the URL
+         * Also the current status for excluding cancers is updated
+         */
+        function update () {
+            otLocationState.setStateFor(stateId, $scope.view);
+        }
+
+
+        /*
+         * Called from the tables in the HTML, this sets the active tab id.
+         * Valid tabs are: 'bubbles', 'table', 'tree'.
+         */
+        $scope.setActiveTab = function (tab) {
+            $scope.view.t[0] = tab;
+            update();
+        };
 
         //
         // on STATECHANGED
@@ -168,6 +253,7 @@ angular.module('otControllers')
         // on PAGE LOAD
         //
         otUtils.clearErrors();
+        setPageFacets();
         $scope.filters = otLocationState.getState()[facetsId] || {};
         render(otLocationState.getState(), otLocationState.getOldState());
     }]);
