ba6f5a2214fdaabbaa4564807aef823d4c24312b
Luca Fumis <lucaf@ebi.ac.uk>
2018-12-10 12:45:38 +0000

    Added sysbio to affected pathway plugin; issue opentargets/platform#358

--- app/plugins/affected-pathway/affected-pathway-directive.js
+++ app/plugins/affected-pathway/affected-pathway-directive.js
@@ -14,16 +14,41 @@ angular.module('otPlugins')
                 ext: '=?'       // optional external object for communication
             },
             link: function (scope) {
-                scope.ext = scope.ext || {};    // object to communicate
-                scope.sources = otConfig.evidence_sources.pathway.map(function (s) {
-                    // so here s is the datasource api 'key' (i.e. lowercase 'pathway')
-                    // now we need to find label and infoUrl from the otConsts.datasources object
-                    var ds = otUtils.getDatasourceById(s);
+                // setup the ext communication object;
+                // this is a little more complex than other plugins because here now we have pathways and system biology
+                scope.ext = scope.ext || {};    // object to communicate;
+                scope.ext.pathway = {};
+                scope.ext.sysbio = {};
 
-                    return {
-                        label: ds.label, // otDictionary[dk[0]],
-                        url: ds.infoUrl // otConsts.dbs_info_url[otConsts.invert(s)]
-                    };
+                scope.sources = {};
+                Object.keys(otConfig.evidence_sources.pathway).forEach(function (p) {
+                    scope.sources[p] = otConfig.evidence_sources.pathway[p].map(function (s) {
+                        // so here s is the datasource api 'key' (i.e. lowercase 'pathway')
+                        // now we need to find label and infoUrl from the otConsts.datasources object
+                        var ds = otUtils.getDatasourceById(s);
+
+                        return {
+                            label: ds.label,
+                            url: ds.infoUrl
+                        };
+                    });
+                });
+
+
+                // setup watchers to update the parent
+
+                scope.$watchGroup([function () { return scope.ext.pathway.isLoading; }, function () { return scope.ext.sysbio.isLoading; }], function () {
+                    scope.ext.isLoading = scope.ext.pathway.isLoading || scope.ext.sysbio.isLoading;
+                });
+
+                scope.$watchGroup([function () { return scope.ext.pathway.hasError; }, function () { return scope.ext.sysbio.hasError; }], function () {
+                    scope.ext.hasError = scope.ext.pathway.hasError || scope.ext.sysbio.hasError;
+                });
+
+                scope.$watchGroup([function () { return scope.ext.pathway.data; }, function () { return scope.ext.sysbio.data; }], function (newdata, olddata) {
+                    if (newdata[0] && newdata[1]) {
+                        scope.ext.data = (newdata[0].length > 0 || newdata[1].length > 0) ? [{}] : [];  // just a fake data object to have a length property for now
+                    }
                 });
             }
         };
