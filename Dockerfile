cded57a9e00f52192cc26a54449116a32d32f7b4
Adam Faulconbridge <afaulconbridge@googlemail.com>
2019-02-15 16:47:55 +0000

    updates to match improved proxy configuration in production

--- Dockerfile
+++ Dockerfile
@@ -10,10 +10,8 @@ COPY ./app /var/www/app/
 COPY ./nginx_conf/server.crt /usr/share/nginx/
 COPY ./nginx_conf/server.key /usr/share/nginx/
 
-#move nginx.conf and template in the right place
-COPY ./nginx_conf/nginx.conf /etc/nginx/nginx.template
-# COPY ./nginx_conf/app_server.template /etc/nginx/conf.d/app_server.template
-# COPY ./nginx_conf/rest_api_scheme.template /etc/nginx/conf.d/rest_api_scheme.template
+#move nginx.template in the right place
+COPY ./nginx_conf/nginx.template /etc/nginx/nginx.template
 
 VOLUME /var/cache/nginx
 
--- docker-entrypoint.sh
+++ docker-entrypoint.sh
@@ -1,17 +1,22 @@
 #!/bin/sh
 
-
+#setup defaults for environment variables
 export REST_API_SCHEME=${REST_API_SCHEME:=https}
-export REST_API_SERVER=${REST_API_SERVER:=api.opentargets.io}
+export REST_API_SERVER=${REST_API_SERVER:=platform-api.opentargets.io}
 export REST_API_PORT=${REST_API_PORT:=443}
+export PROXY_SCHEME=${PROXY_SCHEME:=https}
+export PROXY_SERVER=${PROXY_SERVER:=platform-api.opentargets.io}
+export PROXY_PORT=${PROXY_PORT:=443}
+export PROXY_PATH=${PROXY_PATH:=proxy}
 
 
 # substitute the rest API address in the nginx configuration
 
-envsubst '$REST_API_SCHEME $REST_API_SERVER $REST_API_PORT' < /etc/nginx/nginx.template > /etc/nginx/nginx.conf
+envsubst '$REST_API_SCHEME $REST_API_SERVER $REST_API_PORT $PROXY_SCHEME $PROXY_SERVER $PROXY_PORT $PROXY_PATH' < /etc/nginx/nginx.template > /etc/nginx/nginx.conf
 
 echo "======================================="
 echo "INFO >>> /api is pointing to : ${REST_API_SCHEME}://${REST_API_SERVER}:${REST_API_PORT}"
+echo "INFO >>> /proxy is pointing to : ${PROXY_SCHEME}://${PROXY_SERVER}:${PROXY_PORT}/${PROXY_PATH}"
 echo "======================================="
 
 # assume that user wants to run his own process,
