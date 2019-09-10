f491fc6fd1a3e686e93799c3004335d207a5c42e
Luca Fumis <lucaf@ebi.ac.uk>
2019-09-10 18:51:12 +0100

    Aggregated drugs: create separate plugin; issue opentargets/platform#682

---
+++ app/plugins/drugs-aggregated/drugs-agg-directive.js
@@ -0,0 +1,30 @@
+/**
+ * Drugs plugin
+ */
+angular.module('otPlugins')
+    .directive('otDrugsAgg', ['otConfig', 'otUtils', function (otConfig, otUtils) {
+        'use strict';
+
+        return {
+            restrict: 'E',
+            templateUrl: 'plugins/drugs-aggregated/drugs-agg.html',
+            scope: {
+                target: '=',
+                disease: '=',
+                ext: '=?'       // optional external object for communication
+            },
+            link: function (scope) {
+                scope.ext = scope.ext || {};    // object to communicate
+                scope.sources = otConfig.evidence_sources.known_drug.map(function (s) {
+                    // so here s is the datasource api 'key' (i.e. lowercase 'pathway')
+                    // now we need to find label and infoUrl from the otConsts.datasources object
+                    var ds = otUtils.getDatasourceById(s);
+
+                    return {
+                        label: ds.label, // otDictionary[dk[0]],
+                        url: ds.infoUrl // otConsts.dbs_info_url[otConsts.invert(s)]
+                    };
+                });
+            }
+        };
+    }]);
