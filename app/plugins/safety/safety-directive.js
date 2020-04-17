6a5040965e5f6333e2fda943b4ae46bd9b2c1324
Luca Fumis <lucaf@ebi.ac.uk>
2020-04-17 15:21:33 +0100

    Add tox21 table to safety plugin; issue opentargets/platform#906

--- app/plugins/safety/safety-directive.js
+++ app/plugins/safety/safety-directive.js
@@ -8,6 +8,22 @@ angular.module('otPlugins')
             scope: {
                 target: '='
             },
-            link: function (scope, elem, attrs) {}
+            link: function (scope, elem, attrs) {
+                scope.hasSafetyData = scope.target.safety ? true : false;
+                if (scope.target.safety) {
+                    scope.data = {
+                        adverse_effects: scope.target.safety.adverse_effects,
+                        risk_info: scope.target.safety.safety_risk_info,
+                        tox21: !scope.target.safety.experimental_toxicity ? undefined : scope.target.safety.experimental_toxicity.filter(function (t) {
+                            return t.data_source.toLowerCase() === 'tox21';
+                        }),
+                        etox: !scope.target.safety.experimental_toxicity ? undefined : scope.target.safety.experimental_toxicity.filter(function (t) {
+                            return t.data_source.toLowerCase() === 'etox';
+                        })
+                    };
+                } else {
+                    scope.data = {};
+                }
+            }
         };
     }]);
