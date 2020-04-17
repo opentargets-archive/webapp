9a88e1fcf4770961c40631f2bae17095af480e5c
Luca Fumis <lucaf@ebi.ac.uk>
2020-04-17 16:57:21 +0100

    Add etox table to safety plugin; issue opentargets/platform#906

--- app/plugins/safety/safety-directive.js
+++ app/plugins/safety/safety-directive.js
@@ -14,13 +14,22 @@ angular.module('otPlugins')
                     scope.data = {
                         adverse_effects: scope.target.safety.adverse_effects,
                         risk_info: scope.target.safety.safety_risk_info,
-                        tox21: !scope.target.safety.experimental_toxicity ? undefined : scope.target.safety.experimental_toxicity.filter(function (t) {
+                        tox21: scope.target.safety.experimental_toxicity ? scope.target.safety.experimental_toxicity.filter(function (t) {
                             return t.data_source.toLowerCase() === 'tox21';
-                        }),
-                        etox: !scope.target.safety.experimental_toxicity ? undefined : scope.target.safety.experimental_toxicity.filter(function (t) {
+                        }) : null,
+                        etox: scope.target.safety.experimental_toxicity ? scope.target.safety.experimental_toxicity.filter(function (t) {
                             return t.data_source.toLowerCase() === 'etox';
-                        })
+                        }) : null
                     };
+
+                    // some of the data arrays could be empty,
+                    // so for convenience we do our length check here:
+                    scope.hasData = {
+                        adverse_effects: scope.data.adverse_effects && scope.data.adverse_effects.length > 0,
+                        risk_info: scope.data.risk_info && scope.data.risk_info.length > 0,
+                        tox21: scope.data.tox21 && scope.data.tox21.length > 0,
+                        etox: scope.data.etox && scope.data.etox.length > 0
+                    }
                 } else {
                     scope.data = {};
                 }
